a808c5e2ae3af89a40fbbc4771447ab8f2d94b86
Blake Kaufman <blakekaufman9@gmail.com>
2024-06-09 10:38:48 -0500

    Boltz WSS function

---
+++ app/functions/boltz/handle-submarine-claim-wss.js
@@ -0,0 +1,107 @@
+import {encriptMessage} from '../messaging/encodingAndDecodingMessages';
+import {getBoltzApiUrl} from './boltzEndpoitns';
+
+export default async function handleSubmarineClaimWSS({
+  ref, //reqiured
+  webSocket, //reqiured
+  invoiceAddress, //reqiured
+  swapInfo, //reqiured
+  privateKey, //reqiured
+  toggleMasterInfoObject, //reqiured
+  masterInfoObject, //reqiured
+  contactsPrivateKey, //reqiured
+  refundJSON, //reqiured
+  sendPaymentObj,
+  isReceivingSwapFunc,
+
+  navigate,
+  page,
+}) {
+  console.log('RUNNING IN SUBMARINE CLAIM FUNCTION');
+  return new Promise(resolve => {
+    webSocket.onopen = () => {
+      console.log('did un websocket open');
+      webSocket.send(
+        JSON.stringify({
+          op: 'subscribe',
+          channel: 'swap.update',
+          args: [swapInfo.id],
+        }),
+      );
+    };
+
+    webSocket.onmessage = async rawMsg => {
+      const msg = JSON.parse(rawMsg.data);
+
+      console.log(msg);
+      if (msg.event === 'subscribe') {
+        resolve(true);
+      }
+      if (msg.args[0].status === 'transaction.mempool') {
+        const encripted = encriptMessage(
+          contactsPrivateKey,
+          masterInfoObject.contacts.myProfile.uuid,
+          JSON.stringify(refundJSON),
+        );
+
+        toggleMasterInfoObject({
+          liquidSwaps: [...masterInfoObject.liquidSwaps].concat([encripted]),
+        });
+      } else if (msg.args[0].status === 'invoice.failedToPay') {
+        webSocket.close();
+        if (page === 'loadingScreen') return;
+        navigate.navigate('HomeAdmin');
+        navigate.navigate('ConfirmTxPage', {
+          for: 'paymentFailed',
+          information: {},
+        });
+      } else if (msg.args[0].status === 'transaction.claim.pending') {
+        getClaimSubmarineSwapJS({
+          webViewRef: ref,
+          invoiceAddress,
+          swapInfo,
+          privateKey,
+        });
+      } else if (msg.args[0].status === 'transaction.claimed') {
+        let newLiquidTransactions = [...masterInfoObject.liquidSwaps];
+        newLiquidTransactions.pop();
+
+        toggleMasterInfoObject({
+          liquidSwaps: newLiquidTransactions,
+        });
+
+        webSocket.close();
+        if (page === 'loadingScreen') {
+          navigate.navigate('HomeAdmin');
+        } else {
+          navigate.navigate('HomeAdmin');
+          navigate.navigate('ConfirmTxPage', {
+            for: 'paymentSucceed',
+            information: {},
+          });
+        }
+      }
+    };
+  });
+}
+
+function getClaimSubmarineSwapJS({
+  webViewRef,
+  invoiceAddress,
+  swapInfo,
+  privateKey,
+}) {
+  const args = JSON.stringify({
+    apiUrl: getBoltzApiUrl(process.env.BOLTZ_ENVIRONMENT),
+    network: process.env.BOLTZ_ENVIRONMENT === 'testnet' ? 'testnet' : 'liquid',
+    invoice: invoiceAddress,
+    swapInfo,
+    privateKey,
+  });
+
+  console.log(args, 'WEBVIEW ARGS');
+
+  webViewRef.current.injectJavaScript(
+    `window.claimSubmarineSwap(${args}); void(0);`,
+  );
+}
