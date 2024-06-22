14c1b62bc96f0c5d18ddf11cd521fd5e50cc4d01
Blake Kaufman <blakekaufman9@gmail.com>
2024-06-21 20:42:33 -0500

    updated homepage

--- app/screens/inAccount/home.js
+++ app/screens/inAccount/home.js
@@ -1,38 +1,12 @@
 import {Platform, SafeAreaView, StyleSheet, View} from 'react-native';
 import {useRef} from 'react';
 import {registerWebhook} from '@breeztech/react-native-breez-sdk';
-import NavBar from '../../components/admin/homeComponents/navBar';
 import HomeLightning from '../../components/admin/homeComponents/homeLightning';
 import {ConfigurePushNotifications} from '../../hooks/setNotifications';
-import {listenForMessages} from '../../hooks/listenForMessages';
-import {GlobalThemeView, ThemeText} from '../../functions/CustomElements';
-import CustomFlatList from '../../components/admin/homeComponents/homeLightning/cusomFlatlist/CustomFlatList';
-import {useGlobalContextProvider} from '../../../context-store/context';
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
-
-import {
-  LiquidityIndicator,
-  SendRecieveBTNs,
-  UserSatAmount,
-} from '../../components/admin';
-import {CENTER, COLORS} from '../../constants';
-import getFormattedHomepageTxs, {
-  UserTransaction,
-  dateBanner,
-} from '../../functions/combinedTransactions';
-import {useNavigation} from '@react-navigation/native';
-import {listenForLiquidEvents} from '../../functions/liquidWallet';
 
 export default function AdminHome() {
   console.log('admin home');
   const expoPushToken = ConfigurePushNotifications();
-  const insets = useSafeAreaInsets();
-  const {nodeInformation, masterInfoObject, liquidNodeInformation, theme} =
-    useGlobalContextProvider();
-  const navigate = useNavigation();
-  listenForMessages();
-  listenForLiquidEvents();
-  const showAmount = masterInfoObject.userBalanceDenomination != 'hidden';
 
   const didLogWebhook = useRef(false);
 
@@ -49,68 +23,5 @@ export default function AdminHome() {
       }
     })();
 
-  return (
-    <GlobalThemeView styles={{paddingBottom: 0, paddintTop: 0}}>
-      <CustomFlatList
-        style={{overflow: 'hidden', flex: 1}}
-        data={getFormattedHomepageTxs({
-          nodeInformation,
-          liquidNodeInformation,
-          masterInfoObject,
-          theme,
-          navigate,
-          showAmount,
-        })}
-        renderItem={({item}) => item}
-        HeaderComponent={<NavBar />}
-        StickyElementComponent={
-          <View
-            style={{
-              alignItems: 'center',
-              backgroundColor: theme
-                ? COLORS.darkModeBackground
-                : COLORS.lightModeBackground,
-              paddingBottom: 10,
-            }}>
-            <ThemeText
-              content={'Total Balance'}
-              styles={{
-                textTransform: 'uppercase',
-                // marginTop: 30,
-              }}
-            />
-            <UserSatAmount />
-            <LiquidityIndicator />
-          </View>
-        }
-        TopListElementComponent={
-          <View
-            style={{
-              alignItems: 'center',
-            }}>
-            <SendRecieveBTNs />
-            <ThemeText
-              content={'Transactions'}
-              styles={{
-                paddingBottom: 5,
-              }}
-            />
-          </View>
-        }
-      />
-      {/* </SafeAreaView> */}
-      {/* <NavBar />
-    // <HomeLightning /> */}
-    </GlobalThemeView>
-  );
+  return <HomeLightning />;
 }
-
-const styles = StyleSheet.create({
-  item: {
-    borderColor: 'green',
-    borderWidth: 5,
-    height: 100,
-    marginBottom: 6,
-    width: '100%',
-  },
-});
