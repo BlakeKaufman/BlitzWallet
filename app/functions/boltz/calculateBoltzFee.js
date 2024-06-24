39f7b0f38fbd192ada8d091e3649567161194812
Blake Kaufman <blakekaufman9@gmail.com>
2024-06-24 12:50:06 -0500

    fixed fee logic

--- app/functions/boltz/calculateBoltzFee.js
+++ app/functions/boltz/calculateBoltzFee.js
@@ -4,10 +4,17 @@ export async function calculateBoltzFee(swapAmountSats, swapType) {
   const pairSwapInfo = await getBoltzSwapPairInformation(swapType);
   if (!pairSwapInfo) return new Promise(resolve => resolve(false));
 
-  const fee = Math.round(
-    pairSwapInfo.fees.minerFees +
-      swapAmountSats * (pairSwapInfo.fees.percentage / 100),
-  );
+  const fee =
+    swapType === 'liquid-ln'
+      ? Math.round(
+          pairSwapInfo.fees.minerFees +
+            swapAmountSats * (pairSwapInfo.fees.percentage / 100),
+        )
+      : Math.round(
+          pairSwapInfo.fees.minerFees.claim +
+            pairSwapInfo.fees.minerFees.lockup +
+            swapAmountSats * (pairSwapInfo.fees.percentage / 100),
+        );
 
   return new Promise(resolve => resolve([fee, pairSwapInfo]));
 }
