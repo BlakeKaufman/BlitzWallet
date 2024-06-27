fa514cabe9788e62d7543290c47bfa056b54b2e8
Blake Kaufman <blakekaufman9@gmail.com>
2024-06-26 20:20:08 -0500

    added retry clause to boltz claim function

--- app/functions/boltz/handle-webview-claim-message.js
+++ app/functions/boltz/handle-webview-claim-message.js
@@ -14,55 +14,63 @@ export default function handleWebviewClaimMessage(
       if (data.error) throw Error(data.error);
 
       if (typeof data === 'object' && data?.tx) {
-        console.log(claimTxs);
+        let didPost = false;
+        let numberOfTries = 0;
+        while (!didPost && numberOfTries < 5) {
+          console.log('RUNNING BOLTZ POST');
+          numberOfTries += 1;
+          try {
+            const response = await axios.post(
+              `${getBoltzApiUrl(
+                process.env.BOLTZ_ENVIRONMENT,
+              )}/v2/chain/L-BTC/transaction`,
+              {
+                hex: data.tx,
+              },
+            );
+            didPost = true;
+
+            if (response.data?.id) {
+              if (receiveingPage === 'contactsPage') {
+                navigate.goBack();
+              } else if (receiveingPage === 'receivePage') {
+                navigate.navigate('HomeAdmin');
+                navigate.navigate('ConfirmTxPage', {
+                  for: 'paymentSucceed',
+                  information: {},
+                });
+              } else if (receiveingPage === 'sendingPage') {
+                navigate.navigate('HomeAdmin');
+                navigate.navigate('ConfirmTxPage', {
+                  for: 'paymentSucceed',
+                  information: {},
+                });
+              } else if (receiveingPage === 'POS') {
+                confirmFunction({
+                  invoice: false,
+                  claiming: false,
+                  claimed: true,
+                });
+              } else if (receiveingPage === 'loadingScreen') {
+                navigate.replace('HomeAdmin');
+              }
+            }
+          } catch (err) {
+            console.log(err);
+            if (receiveingPage === 'loadingScreen') {
+              confirmFunction(1);
+            }
+          }
+        }
+
+        if (didPost) return;
+
         let claimTxs =
           JSON.parse(await getLocalStorageItem('boltzClaimTxs')) || [];
 
         claimTxs.push([data.tx, new Date()]);
 
         setLocalStorageItem('boltzClaimTxs', JSON.stringify(claimTxs));
-
-        try {
-          const response = await axios.post(
-            `${getBoltzApiUrl(
-              process.env.BOLTZ_ENVIRONMENT,
-            )}/v2/chain/L-BTC/transaction`,
-            {
-              hex: data.tx,
-            },
-          );
-
-          if (response.data?.id) {
-            if (receiveingPage === 'contactsPage') {
-              navigate.goBack();
-            } else if (receiveingPage === 'receivePage') {
-              navigate.navigate('HomeAdmin');
-              navigate.navigate('ConfirmTxPage', {
-                for: 'paymentSucceed',
-                information: {},
-              });
-            } else if (receiveingPage === 'sendingPage') {
-              navigate.navigate('HomeAdmin');
-              navigate.navigate('ConfirmTxPage', {
-                for: 'paymentSucceed',
-                information: {},
-              });
-            } else if (receiveingPage === 'POS') {
-              confirmFunction({
-                invoice: false,
-                claiming: false,
-                claimed: true,
-              });
-            } else if (receiveingPage === 'loadingScreen') {
-              navigate.replace('HomeAdmin');
-            }
-          }
-        } catch (err) {
-          console.log(err);
-          if (receiveingPage === 'loadingScreen') {
-            confirmFunction(1);
-          }
-        }
       }
     } catch (err) {
       console.log(err, 'WEBVIEW ERROR');
