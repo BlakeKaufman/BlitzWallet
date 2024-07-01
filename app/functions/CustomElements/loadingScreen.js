cb4131f13ca020073542e0fd752d0958fe1a28d6
Blake Kaufman <blakekaufman9@gmail.com>
2024-07-01 13:17:21 -0400

    crated loading screen component

---
+++ app/functions/CustomElements/loadingScreen.js
@@ -0,0 +1,41 @@
+import {ActivityIndicator, StyleSheet, View} from 'react-native';
+import {useGlobalContextProvider} from '../../../context-store/context';
+import {COLORS} from '../../constants';
+import ThemeText from './textTheme';
+
+export default function FullLoadingScreen({
+  text,
+  containerStyles,
+  reversed,
+  textStyles,
+}) {
+  const {theme} = useGlobalContextProvider();
+  return (
+    <View style={[styles.container, {containerStyles}]}>
+      <ActivityIndicator
+        color={
+          theme
+            ? reversed
+              ? COLORS.lightModeText
+              : COLORS.darkModeText
+            : reversed
+            ? COLORS.darkModeText
+            : COLORS.lightModeText
+        }
+        size={'large'}
+      />
+      <ThemeText styles={{...styles.text, ...textStyles}} content={text} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    marginTop: 10,
+  },
+});
