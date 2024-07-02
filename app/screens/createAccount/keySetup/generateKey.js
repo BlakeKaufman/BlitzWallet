29f88c49093501043607d4bcff6c2f5d6ef02d25
Blake Kaufman <blakekaufman9@gmail.com>
2024-07-02 15:28:26 -0400

    update page to new safearea view

--- app/screens/createAccount/keySetup/generateKey.js
+++ app/screens/createAccount/keySetup/generateKey.js
@@ -9,20 +9,18 @@ import {
   ActivityIndicator,
 } from 'react-native';
 import {Back_BTN, KeyContainer} from '../../../components/login';
-import {Background, COLORS, FONT, SHADOWS, SIZES} from '../../../constants';
+import {COLORS, FONT, SIZES} from '../../../constants';
 import {useState} from 'react';
-import {storeData, retrieveData} from '../../../functions/secureStore';
+import {retrieveData} from '../../../functions/secureStore';
 import generateMnemnoic from '../../../functions/seed';
-import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {useTranslation} from 'react-i18next';
 import {useGlobalContextProvider} from '../../../../context-store/context';
-import {ANDROIDSAFEAREA} from '../../../constants/styles';
+import {GlobalThemeView} from '../../../functions/CustomElements';
 
 export default function GenerateKey({navigation: {navigate}}) {
   const {setContactsPrivateKey} = useGlobalContextProvider();
   const [mnemonic, setMnemonic] = useState([]);
   const [fetchError, setFetchError] = useState(false);
-  const insets = useSafeAreaInsets();
   const {t} = useTranslation();
 
   useState(async () => {
@@ -40,86 +38,74 @@ export default function GenerateKey({navigation: {navigate}}) {
   }, []);
 
   return (
-    <View style={Background}>
-      <View
-        style={[
-          styles.global_container,
-          {
-            marginTop: insets.top === 0 ? ANDROIDSAFEAREA : insets.top,
-            marginBottom: insets.bottom === 0 ? ANDROIDSAFEAREA : insets.bottom,
-          },
-        ]}>
-        <Back_BTN navigation={navigate} destination="StartKeyGeneration" />
-        <View style={styles.container}>
-          <Text style={styles.header}>
-            {t('createAccount.generateKeyPage.header')}
-          </Text>
-          <Text style={styles.subHeader}>
-            {t('createAccount.generateKeyPage.subHeader')}
-          </Text>
-          {!fetchError ? (
-            mnemonic.length != 0 ? (
-              <View style={{flex: 1, paddingBottom: 10}}>
-                <ScrollView>
-                  <KeyContainer keys={mnemonic} />
-                </ScrollView>
-              </View>
-            ) : (
-              <ActivityIndicator
-                size="large"
-                style={{marginTop: 'auto', marginBottom: 'auto'}}
-              />
-            )
-          ) : (
-            <View
-              style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-              <Text style={[styles.text, {color: COLORS.lightModeText}]}>
-                {t('createAccount.generateKeyPage.errorText')}
-              </Text>
+    <GlobalThemeView>
+      <Back_BTN navigation={navigate} destination="StartKeyGeneration" />
+      <View style={styles.container}>
+        <Text style={styles.header}>
+          {t('createAccount.generateKeyPage.header')}
+        </Text>
+        <Text style={styles.subHeader}>
+          {t('createAccount.generateKeyPage.subHeader')}
+        </Text>
+        {!fetchError ? (
+          mnemonic.length != 0 ? (
+            <View style={{flex: 1, paddingBottom: 10}}>
+              <ScrollView>
+                <KeyContainer keys={mnemonic} />
+              </ScrollView>
             </View>
-          )}
+          ) : (
+            <ActivityIndicator
+              size="large"
+              style={{marginTop: 'auto', marginBottom: 'auto'}}
+            />
+          )
+        ) : (
           <View
-            style={{
-              width: '90%',
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-            }}>
-            <TouchableOpacity
-              onPress={() => {
-                navigate('PinSetup', {isInitialLoad: true});
-              }}
-              style={[
-                styles.button,
-                {
-                  backgroundColor: 'transparent',
-                  borderColor: COLORS.primary,
-                  borderWidth: 2,
-                },
-              ]}>
-              <Text style={[styles.text, {color: COLORS.lightModeText}]}>
-                {t('createAccount.generateKeyPage.button1')}
-              </Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => {
-                navigate('VerifyKey');
-              }}
-              style={styles.button}>
-              <Text style={styles.text}>
-                {t('createAccount.generateKeyPage.button2')}
-              </Text>
-            </TouchableOpacity>
+            style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <Text style={[styles.text, {color: COLORS.lightModeText}]}>
+              {t('createAccount.generateKeyPage.errorText')}
+            </Text>
           </View>
+        )}
+        <View
+          style={{
+            width: '90%',
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+          }}>
+          <TouchableOpacity
+            onPress={() => {
+              navigate('PinSetup', {isInitialLoad: true});
+            }}
+            style={[
+              styles.button,
+              {
+                backgroundColor: 'transparent',
+                borderColor: COLORS.primary,
+                borderWidth: 2,
+              },
+            ]}>
+            <Text style={[styles.text, {color: COLORS.lightModeText}]}>
+              {t('createAccount.generateKeyPage.button1')}
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              navigate('VerifyKey');
+            }}
+            style={styles.button}>
+            <Text style={styles.text}>
+              {t('createAccount.generateKeyPage.button2')}
+            </Text>
+          </TouchableOpacity>
         </View>
       </View>
-    </View>
+    </GlobalThemeView>
   );
 }
 
 const styles = StyleSheet.create({
-  global_container: {
-    flex: 1,
-  },
   container: {
     width: '95%',
     flex: 1,
