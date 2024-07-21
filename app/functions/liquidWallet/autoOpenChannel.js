5d69be6ed75c8703af907bd312e79afe8442c8ca
Blake Kaufman <blakekaufman9@gmail.com>
2024-07-21 13:39:23 -0500

    updated function to use global liquid buffer

--- app/functions/liquidWallet/autoOpenChannel.js
+++ app/functions/liquidWallet/autoOpenChannel.js
@@ -3,60 +3,62 @@ import {
   receivePayment,
 } from '@breeztech/react-native-breez-sdk';
 import createLiquidToLNSwap from '../boltz/liquidToLNSwap';
+import {LIQUIDAMOUTBUFFER} from '../../constants/math';
 
-export default async function autoOpenChannel(
-  nodeInformation,
+export default async function autoOpenChannel({
   liquidNodeInformation,
   masterInfoObject,
-  toggleMasterInfoObject,
-) {
-  if (!masterInfoObject.liquidWalletSettings.regulateChannelOpen)
-    return new Promise(resolve => resolve(false));
-
-  if (
-    liquidNodeInformation.userBalance <
-    masterInfoObject.liquidWalletSettings.regulatedChannelOpenSize + 500
-  )
-    return new Promise(resolve => resolve(false));
-
-  console.log('RUN');
-
-  const channelOpenFee = await openChannelFee({
-    amountMsat:
-      masterInfoObject.liquidWalletSettings.regulatedChannelOpenSize * 1000,
-  });
-
-  let maxChannelOpenFee =
-    masterInfoObject.liquidWalletSettings?.maxChannelOpenFee || 5000; //for legacy users that might not have changed liquid seetings page
-
-  if (maxChannelOpenFee < channelOpenFee.feeMsat / 1000)
-    return new Promise(resolve => resolve(false));
-
-  const invoice = await receivePayment({
-    amountMsat:
-      masterInfoObject.liquidWalletSettings.regulatedChannelOpenSize * 1000,
-    description: 'Auto Channel Open',
-  });
-
-  if (invoice) {
-    // {swapInfo, privateKey}
-    const liquidLNSwapResponse = await createLiquidToLNSwap(
-      invoice.lnInvoice.bolt11,
-    );
-
-    if (liquidLNSwapResponse) {
-      return new Promise(resolve =>
-        resolve({
-          swapInfo: liquidLNSwapResponse.swapInfo,
-          privateKey: liquidLNSwapResponse.privateKey,
-          invoice: invoice.lnInvoice.bolt11,
-          didWork: true,
-        }),
-      );
-    } else {
+}) {
+  try {
+    if (!masterInfoObject.liquidWalletSettings.regulateChannelOpen)
       return new Promise(resolve => resolve(false));
+
+    if (
+      liquidNodeInformation.userBalance - LIQUIDAMOUTBUFFER <
+      masterInfoObject.liquidWalletSettings.regulatedChannelOpenSize
+    )
+      return new Promise(resolve => resolve(false));
+
+    const channelOpenFee = await openChannelFee({
+      amountMsat:
+        masterInfoObject.liquidWalletSettings.regulatedChannelOpenSize * 1000,
+    });
+
+    let maxChannelOpenFee =
+      masterInfoObject.liquidWalletSettings?.maxChannelOpenFee || 5000; //for legacy users that might not have changed liquid seetings page
+
+    if (maxChannelOpenFee < channelOpenFee.feeMsat / 1000)
+      return new Promise(resolve => resolve(false));
+
+    const invoice = await receivePayment({
+      amountMsat:
+        masterInfoObject.liquidWalletSettings.regulatedChannelOpenSize * 1000,
+      description: 'Auto Channel Open',
+    });
+
+    if (invoice) {
+      // {swapInfo, privateKey}
+      const liquidLNSwapResponse = await createLiquidToLNSwap(
+        invoice.lnInvoice.bolt11,
+      );
+
+      if (liquidLNSwapResponse) {
+        return new Promise(resolve =>
+          resolve({
+            swapInfo: liquidLNSwapResponse.swapInfo,
+            privateKey: liquidLNSwapResponse.privateKey,
+            invoice: invoice.lnInvoice.bolt11,
+            didWork: true,
+          }),
+        );
+      } else {
+        return new Promise(resolve => resolve(false));
+      }
+    } else {
+      new Promise(resolve => resolve(false));
     }
-  } else {
+  } catch (err) {
+    console.log(err, 'in auto open');
     new Promise(resolve => resolve(false));
   }
 }
