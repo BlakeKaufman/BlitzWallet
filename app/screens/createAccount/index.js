0ec5db398c6da8795042016869cc81b950da1da8
Blake Kaufman <blakekaufman9@gmail.com>
2024-07-26 18:51:44 -0500

    updated active pages

--- app/screens/createAccount/index.js
+++ app/screens/createAccount/index.js
@@ -2,18 +2,20 @@ import CreateAccountHome from './home';
 import DislaimerPage from './disclaimer';
 import GenerateKey from './keySetup/generateKey';
 import PinSetupPage from './keySetup/pin';
-import SecuityOption from './keySetup/secrityOption';
-import VerifyKey from './keySetup/verifyKey';
+// import SecuityOption from './keySetup/secrityOption';
+// import VerifyKey from './keySetup/verifyKey';
 import RestoreWallet from './restoreWallet/home';
 import RestoreWalletError from './restoreWallet/errorScreen';
+import SkipCreateAccountPathMessage from './skipMessage';
 
 export {
   CreateAccountHome,
   DislaimerPage,
   GenerateKey,
   PinSetupPage,
-  SecuityOption,
-  VerifyKey,
+  // SecuityOption,
+  // VerifyKey,
   RestoreWallet,
   RestoreWalletError,
+  SkipCreateAccountPathMessage,
 };
