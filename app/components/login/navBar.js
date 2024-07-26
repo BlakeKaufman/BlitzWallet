fa4a0ddce45f04bc09bfd7a61c4c52d954aa1271
Blake Kaufman <blakekaufman9@gmail.com>
2024-07-25 21:02:42 -0500

    create new login path nav bar

---
+++ app/components/login/navBar.js
@@ -0,0 +1,33 @@
+import {useNavigation} from '@react-navigation/native';
+import {StyleSheet, View} from 'react-native';
+import Back_BTN from './back_BTN';
+import CustomButton from '../../functions/CustomElements/button';
+import {SIZES} from '../../constants';
+
+export default function LoginNavbar({destination}) {
+  const navigate = useNavigation();
+  return (
+    <View style={styles.container}>
+      <Back_BTN navigation={navigate.navigate} destination={destination} />
+      <CustomButton
+        buttonStyles={{
+          width: 'auto',
+        }}
+        textStyles={{
+          fontSize: SIZES.large,
+        }}
+        textContent={'Skip all'}
+        // actionFunction={() => }
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    alignItems: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+});
