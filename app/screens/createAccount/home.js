8c010049ee498455e1808b5062efc30bf4f1a6ce
Blake Kaufman <blakekaufman9@gmail.com>
2024-07-27 16:44:52 -0500

    updated button placement

--- app/screens/createAccount/home.js
+++ app/screens/createAccount/home.js
@@ -19,10 +19,10 @@ import {
 } from '../../constants';
 import {useTranslation} from 'react-i18next';
 import {generateMnemnoic, storeData} from '../../functions';
-import {deleteItem} from '../../functions/secureStore';
 import {useGlobalContextProvider} from '../../../context-store/context';
 import {GlobalThemeView, ThemeText} from '../../functions/CustomElements';
 import CustomButton from '../../functions/CustomElements/button';
+import {WINDOWWIDTH} from '../../constants/theme';
 
 export default function CreateAccountHome({navigation: {navigate}}) {
   const {t} = useTranslation();
@@ -37,42 +37,27 @@ export default function CreateAccountHome({navigation: {navigate}}) {
   return (
     <GlobalThemeView>
       <View style={styles.container}>
-        {/* <View style={styles.logo}>
-          <Image
-            source={ICONS.logoIcon}
-            style={{width: '100%', height: '100%'}}
-          />
-        </View> */}
-        <ThemeText
-          styles={{
-            fontSize: 70,
-            fontStyle: 'italic',
-            fontWeight: '800',
-            color: COLORS.primary,
-            marginBottom: 30,
-            marginTop: 'auto',
-          }}
-          content={'Blitz'}
-        />
-        {/* <Text style={styles.title}>Blitz</Text> */}
-        {/* <Text style={styles.sub_title}>
-          {t('createAccount.homePage.title')}
-        </Text> */}
+        <ThemeText styles={styles.blitz} content={'Blitz'} />
+
         <CustomButton
           buttonStyles={{
             width: '80%',
-            backgroundColor: COLORS.primary,
             marginBottom: 20,
+            backgroundColor: COLORS.primary,
           }}
           textStyles={{...styles.buttonText, color: COLORS.darkModeText}}
-          textContent={t('createAccount.homePage.buttons.button1')}
-          actionFunction={() => navigate('RestoreWallet')}
-        />
-        <CustomButton
-          buttonStyles={{width: '80%', marginBottom: 20}}
-          textStyles={{...styles.buttonText, color: COLORS.lightModeText}}
           textContent={t('createAccount.homePage.buttons.button2')}
           actionFunction={() => navigate('DisclaimerPage')}
+        />
+        <CustomButton
+          buttonStyles={{
+            width: '80%',
+
+            marginBottom: 20,
+          }}
+          textStyles={{...styles.buttonText, color: COLORS.lightModeText}}
+          textContent={t('createAccount.homePage.buttons.button1')}
+          actionFunction={() => navigate('RestoreWallet')}
         />
         <CustomButton
           buttonStyles={{width: '80%', backgroundColor: 'transparent'}}
@@ -80,34 +65,6 @@ export default function CreateAccountHome({navigation: {navigate}}) {
           textContent={t('createAccount.homePage.buttons.button3')}
           actionFunction={() => navigate('RedeemGiftScreen')}
         />
-        {/* <TouchableOpacity
-          style={[styles.button_empty]}
-          onPress={() => {
-            navigate('RedeemGiftScreen');
-          }}>
-          <Text
-            style={[styles.button_empty_text, {color: COLORS.lightModeText}]}>
-            Receive Gift
-          </Text>
-        </TouchableOpacity> */}
-        {/* <TouchableOpacity
-          style={[BTN, {backgroundColor: COLORS.primary}]}
-          onPress={() => {
-            navigate('DisclaimerPage');
-          }}>
-          <Text style={styles.button_full_text}>
-            {t('createAccount.homePage.buttons.button1')}
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={[styles.button_empty, {marginBottom: 'auto'}]}
-          onPress={() => {
-            navigate('RestoreWallet');
-          }}>
-          <Text style={styles.button_empty_text}>
-            {t('createAccount.homePage.buttons.button2')}
-          </Text>
-        </TouchableOpacity> */}
 
         <ThemeText
           styles={{...styles.disclamer_text}}
@@ -119,61 +76,29 @@ export default function CreateAccountHome({navigation: {navigate}}) {
 }
 
 const styles = StyleSheet.create({
-  background: {
-    flex: 1,
-  },
   container: {
     flex: 1,
-    width: '95%',
+    width: WINDOWWIDTH,
 
     flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
     ...CENTER,
   },
-  logo: {
-    width: 100,
-    height: 100,
+  blitz: {
+    fontSize: 80,
+    fontStyle: 'italic',
+    fontWeight: '800',
+    color: COLORS.primary,
+    marginBottom: 30,
     marginTop: 'auto',
-    marginBottom: 20,
+    includeFontPadding: false,
   },
-  title: {
-    color: COLORS.secondary,
-    fontSize: SIZES.huge,
-    marginBottom: 10,
-    fontFamily: FONT.Title_Regular,
-  },
-  sub_title: {
-    maxWidth: '95%',
-    color: COLORS.secondary,
-    fontSize: SIZES.large,
-    textAlign: 'center',
-    fontFamily: FONT.Other_Regular,
-  },
-  button_full: {
-    backgroundColor: COLORS.primary,
-    width: '100%',
-    maxWidth: 300,
-    height: 50,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: 50,
-    borderRadius: 5,
-  },
-  button_full_text: {
-    color: COLORS.white,
-    fontSize: SIZES.large,
-    fontFamily: FONT.Other_Regular,
-  },
-  button_empty: {
-    width: '100%',
-    maxWidth: 300,
-    marginTop: 30,
-  },
+
   buttonText: {
     fontSize: SIZES.large,
   },
+
   disclamer_text: {
     marginTop: 'auto',
     fontSize: SIZES.small,
