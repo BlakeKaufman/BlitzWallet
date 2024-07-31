ee5398ab54616e48548b343b9545133e77f58059
Blake Kaufman <blakekaufman9@gmail.com>
2024-07-31 17:00:23 -0500

    added breezTest file to app index for easy use

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import app from './app.config';
+// import BreezTest from './app/screens/breezTest';
 // import './i18n'; // for translation option
 
 AppRegistry.registerComponent(app.name, () => App);
