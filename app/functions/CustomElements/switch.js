e1dc2338690ebb60653a440fb78e8525e678aef1
Blake Kaufman <blakekaufman9@gmail.com>
2024-08-04 20:01:31 -0500

    created custom switch

---
+++ app/functions/CustomElements/switch.js
@@ -0,0 +1,84 @@
+import React, {useState, useRef, useEffect} from 'react';
+import {View, Text, TouchableOpacity, StyleSheet, Animated} from 'react-native';
+import {COLORS, SIZES} from '../../constants';
+import {useGlobalContextProvider} from '../../../context-store/context';
+
+const CustomToggleSwitch = ({page}) => {
+  const {masterInfoObject, toggleMasterInfoObject} = useGlobalContextProvider();
+  console.log(masterInfoObject.enabledSlidingCamera);
+  const isOn = masterInfoObject.enabledSlidingCamera;
+  const animatedValue = useRef(new Animated.Value(0)).current;
+
+  useEffect(() => {
+    Animated.timing(animatedValue, {
+      toValue: isOn ? 1 : 0,
+      duration: 300, // Duration of the animation
+      useNativeDriver: false, // Enable if animating style properties
+    }).start();
+  }, [isOn]);
+
+  const toggleSwitch = () =>
+    toggleMasterInfoObject({
+      enabledSlidingCamera: !masterInfoObject.enabledSlidingCamera,
+    });
+
+  const switchColor = animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: [COLORS.darkModeBackground, COLORS.primary], // From inactive to active color
+  });
+
+  const circlePosition = animatedValue.interpolate({
+    inputRange: [0, 1],
+    outputRange: [5, 40], // From left to right position
+  });
+
+  return (
+    <TouchableOpacity onPress={toggleSwitch} activeOpacity={0.7}>
+      <Animated.View style={[styles.switch, {backgroundColor: switchColor}]}>
+        <Animated.View style={[styles.circle, {left: circlePosition}]} />
+        <Animated.Text
+          style={[
+            styles.text,
+            {
+              left: circlePosition,
+              transform: [
+                {
+                  translateX: animatedValue.interpolate({
+                    inputRange: [0, 1],
+                    outputRange: [30, -28], // Adjust text position relative to the circle
+                  }),
+                },
+              ],
+            },
+          ]}>
+          {isOn ? 'ON' : 'OFF'}
+        </Animated.Text>
+      </Animated.View>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  switch: {
+    width: 70,
+    height: 35,
+    borderRadius: 20,
+    justifyContent: 'center',
+    // paddingHorizontal: 20,
+  },
+  circle: {
+    width: 24,
+    height: 24,
+    borderRadius: 12,
+    backgroundColor: '#fff',
+    position: 'absolute',
+  },
+  text: {
+    position: 'absolute',
+    fontSize: SIZES.small,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+});
+
+export default CustomToggleSwitch;
