59a66f00adc3f466bb8cf32a16ac74801da95bf6
Blake Kaufman <blakekaufman9@gmail.com>
2024-08-07 20:11:32 -0500

    updated warning text to new sat text

--- app/components/admin/homeComponents/sendBitcoin/components/warningText.js
+++ app/components/admin/homeComponents/sendBitcoin/components/warningText.js
@@ -2,6 +2,7 @@ import {StyleSheet, View} from 'react-native';
 import {ThemeText} from '../../../../../functions/CustomElements';
 import {formatBalanceAmount, numberConverter} from '../../../../../functions';
 import {useGlobalContextProvider} from '../../../../../../context-store/context';
+import FormattedSatText from '../../../../../functions/CustomElements/satTextDisplay';
 
 export default function TransactionWarningText({
   canSendPayment,
@@ -20,9 +21,102 @@ export default function TransactionWarningText({
   //  Lighting with zero invoice
   //  Lightning with LNURL pay
   //  Lightning
+
+  console.log(minMaxLiquidSwapAmounts, !sendingAmount);
+  const textItem = (() => {
+    if (!canSendPayment && sendingAmount)
+      return (
+        <ThemeText
+          styles={{includeFontPadding: false}}
+          content={'Cannot send Payment'}
+        />
+      );
+
+    if (!sendingAmount)
+      return (
+        <ThemeText
+          styles={{includeFontPadding: false}}
+          content={'Please enter a send amount'}
+        />
+      );
+
+    if (isLightningPayment) {
+      if (canUseLightning) {
+        return <ThemeText content={''} />;
+      } else if (isUsingLiquidWithZeroInvoice) {
+        return (
+          <ThemeText
+            styles={{includeFontPadding: false}}
+            content={
+              'Zero Amount Invoices are not allowed when paying from the bank'
+            }
+          />
+        );
+      } else if (canUseLiquid) {
+        return (
+          <FormattedSatText
+            frontText={`Minimum swap amount `}
+            neverHideBalance={true}
+            iconHeight={15}
+            iconWidth={15}
+            styles={{includeFontPadding: false}}
+            formattedBalance={formatBalanceAmount(
+              numberConverter(
+                minMaxLiquidSwapAmounts.min,
+                masterInfoObject.userBalanceDenomination,
+                nodeInformation,
+                masterInfoObject.userBalanceDenomination === 'fiat' ? 2 : 0,
+              ),
+            )}
+          />
+        );
+      } else return <ThemeText content={'Cannot send Payment'} />;
+    } else {
+      console.log(canUseLiquid, canUseLightning);
+      if (canUseLiquid || canUseLightning) {
+        if (
+          sendingAmount >
+          (canUseLiquid
+            ? 1000
+            : minMaxLiquidSwapAmounts.submarineSwapStats?.limits?.minimal ||
+              1000)
+        ) {
+          return <ThemeText content={''} />;
+        }
+        return (
+          <FormattedSatText
+            frontText={`Minimum swap amount `}
+            neverHideBalance={true}
+            iconHeight={15}
+            iconWidth={15}
+            styles={{includeFontPadding: false}}
+            formattedBalance={formatBalanceAmount(
+              numberConverter(
+                canUseLiquid
+                  ? 1000
+                  : minMaxLiquidSwapAmounts.submarineSwapStats?.limits
+                      ?.minimal || 1000,
+                masterInfoObject.userBalanceDenomination,
+                nodeInformation,
+                masterInfoObject.userBalanceDenomination === 'fiat' ? 2 : 0,
+              ),
+            )}
+          />
+        );
+      } else
+        return (
+          <ThemeText
+            styles={{includeFontPadding: false}}
+            content={'Cannot send Payment'}
+          />
+        );
+    }
+  })();
+  console.log(textItem);
   return (
     <View style={styles.container}>
-      <ThemeText
+      {textItem}
+      {/* <ThemeText
         styles={{...styles.warningText}}
         content={
           canSendPayment
@@ -78,7 +172,7 @@ export default function TransactionWarningText({
             ? 'Please enter a send amount'
             : 'Cannot send payment'
         }
-      />
+      /> */}
     </View>
   );
 }
@@ -87,6 +181,7 @@ const styles = StyleSheet.create({
   container: {
     marginTop: 'auto',
     marginBottom: 10,
+    alignItems: 'center',
   },
   warningText: {
     textAlign: 'center',
