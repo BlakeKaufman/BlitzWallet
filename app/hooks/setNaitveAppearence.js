99d897305cc9897762a95225dc115c3e153daa68
Blake Kaufman <blakekaufman9@gmail.com>
2024-08-20 16:54:18 -0400

    updated native color scheme hook to return style

--- app/hooks/setNaitveAppearence.js
+++ app/hooks/setNaitveAppearence.js
@@ -1,13 +1,16 @@
 import {useEffect, useState} from 'react';
 import {Appearance} from 'react-native';
-
 export default function SetNaitveAppearence() {
+  const [nativeColorScheme, setNativeColorScheme] = useState(
+    Appearance.getColorScheme(),
+  );
   useEffect(() => {
     const subscription = Appearance.addChangeListener(({colorScheme}) => {
-      setStatusBarStyle(colorScheme === 'dark' ? 'light' : 'dark');
+      setNativeColorScheme(colorScheme === 'dark' ? 'light' : 'dark');
     });
 
     // Clean up the subscription on unmount
     return () => subscription.remove();
   }, []);
+  return nativeColorScheme;
 }
