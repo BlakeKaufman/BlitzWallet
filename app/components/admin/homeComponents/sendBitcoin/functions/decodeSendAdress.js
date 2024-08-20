762cfa18236bd36c2aa1a5e98f4be10f1e59ea55
Blake Kaufman <blakekaufman9@gmail.com>
2024-08-20 17:07:57 -0400

    fixed decode send adress function

--- app/components/admin/homeComponents/sendBitcoin/functions/decodeSendAdress.js
+++ app/components/admin/homeComponents/sendBitcoin/functions/decodeSendAdress.js
@@ -14,8 +14,9 @@ import createLNToLiquidSwap from '../../../../../functions/boltz/LNtoLiquidSwap'
 import handleReverseClaimWSS from '../../../../../functions/boltz/handle-reverse-claim-wss';
 import {getBoltzWsUrl} from '../../../../../functions/boltz/boltzEndpoitns';
 import axios from 'axios';
+import bip39LiquidAddressDecode from './bip39LiquidAddressDecode';
 
-async function decodeSendAddress({
+export default async function decodeSendAddress({
   nodeInformation,
   btcAdress,
   goBackFunction,
@@ -292,50 +293,50 @@ async function setupLNPage({
   }
 }
 
-function bip39LiquidAddressDecode(btcAddress) {
-  const isBip21 = btcAddress.startsWith(
-    process.env.BOLTZ_ENVIRONMENT === 'testnet'
-      ? 'liquidtestnet:'
-      : 'liquidnetwork:',
-  );
+// function bip39LiquidAddressDecode(btcAddress) {
+//   const isBip21 = btcAddress.startsWith(
+//     process.env.BOLTZ_ENVIRONMENT === 'testnet'
+//       ? 'liquidtestnet:'
+//       : 'liquidnetwork:',
+//   );
 
-  let addressInfo = {};
+//   let addressInfo = {};
 
-  if (isBip21) {
-    const [address, paymentInfo] = btcAddress.split('?');
+//   if (isBip21) {
+//     const [address, paymentInfo] = btcAddress.split('?');
 
-    const parsedAddress = address.split(':')[1];
+//     const parsedAddress = address.split(':')[1];
 
-    paymentInfo.split('&').forEach(data => {
-      const [label, information] = data.split('=');
-      if (label === 'amount') {
-        addressInfo[label] = String(
-          Math.round(
-            information > 500
-              ? information * 1000
-              : information * SATSPERBITCOIN * 1000,
-          ),
-        );
-        return;
-      } else if (label === 'label') {
-        addressInfo[label] = decodeURIComponent(information);
-        return;
-      }
+//     paymentInfo.split('&').forEach(data => {
+//       const [label, information] = data.split('=');
+//       if (label === 'amount') {
+//         addressInfo[label] = String(
+//           Math.round(
+//             information > 500
+//               ? information * 1000
+//               : information * SATSPERBITCOIN * 1000,
+//           ),
+//         );
+//         return;
+//       } else if (label === 'label') {
+//         addressInfo[label] = decodeURIComponent(information);
+//         return;
+//       }
 
-      addressInfo[label] = information;
-    });
+//       addressInfo[label] = information;
+//     });
 
-    addressInfo['isBip21'] = true;
-    addressInfo['address'] = parsedAddress;
-  } else {
-    addressInfo['address'] = btcAddress;
-    addressInfo['amount'] = '';
-    addressInfo['label'] = null;
-    addressInfo['isBip21'] = false;
-    addressInfo['assetid'] = assetIDS['L-BTC'];
-  }
+//     addressInfo['isBip21'] = true;
+//     addressInfo['address'] = parsedAddress;
+//   } else {
+//     addressInfo['address'] = btcAddress;
+//     addressInfo['amount'] = '';
+//     addressInfo['label'] = null;
+//     addressInfo['isBip21'] = false;
+//     addressInfo['assetid'] = assetIDS['L-BTC'];
+//   }
 
-  return addressInfo;
-}
+//   return addressInfo;
+// }
 
-export {bip39LiquidAddressDecode, decodeSendAddress};
+// export default {bip39LiquidAddressDecode, decodeSendAddress};
