ff9a5e1816a2e385a558f6d524f14d774f6d45c2
Blake Kaufman <blakekaufman9@gmail.com>
2024-08-20 17:00:04 -0400

    removed keyboard autofocus

--- app/components/admin/homeComponents/homeLightning/manualEnterSendAddress.js
+++ app/components/admin/homeComponents/homeLightning/manualEnterSendAddress.js
@@ -61,7 +61,6 @@ export default function ManualEnterSendAddress() {
               },
             ]}
             multiline
-            autoFocus={true}
             onChangeText={setInputValue}
             value={inputValue}
             textAlignVertical="top"
