e17015a413ca14f4954fce1a586128002230b58f
Blake Kaufman <blakekaufman9@gmail.com>
2024-08-21 20:50:27 -0400

    updated tabs styling

--- navigation/tabs/index.js
+++ navigation/tabs/index.js
@@ -63,170 +63,139 @@ function MyTabBar({state, descriptors, navigation}) {
   }, [deepLinkContent]);
 
   return (
-    <View
-      style={{
-        width: '100%',
-
-        borderTopColor: theme
-          ? COLORS.darkModeBackgroundOffset
-          : COLORS.lightModeBackgroundOffset,
-
-        borderTopRightRadius: 8,
-        borderTopLeftRadius: 8,
-      }}>
+    <View>
       <View
         style={[
           {
-            width: useWindowDimensions().width * 0.99,
+            width: useWindowDimensions().width,
             backgroundColor: theme
               ? COLORS.darkModeBackgroundOffset
               : COLORS.lightModeBackgroundOffset,
             left: (useWindowDimensions().width * 0.01) / 2,
-            height: 3,
+            height: 40,
             position: 'absolute',
-            top: 0,
+            left: 0,
+            top: -5,
             zIndex: -1,
-            borderTopLeftRadius: 30,
-            borderTopRightRadius: 30,
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
           },
         ]}></View>
       <View
         style={{
-          width: 300,
-          // flex: 1,
-          paddingBottom: insets.bottom < 20 ? ANDROIDSAFEAREA : insets.bottom,
-          paddingTop: 10,
-          flexDirection: 'row',
-          ...CENTER,
+          width: '100%',
+
+          backgroundColor: theme
+            ? COLORS.darkModeBackground
+            : COLORS.lightModeBackground,
+
+          borderTopRightRadius: 15,
+          borderTopLeftRadius: 15,
+          zIndex: 1,
         }}>
-        {state.routes.map((route, index) => {
-          const {options} = descriptors[route.key];
-          const label =
-            options.tabBarLabel !== undefined
-              ? options.tabBarLabel
-              : options.title !== undefined
-              ? options.title
-              : route.name === 'ContactsPageInit'
-              ? 'Contacts'
-              : route.name;
+        <View
+          style={{
+            width: 300,
+            paddingBottom: insets.bottom < 20 ? ANDROIDSAFEAREA : insets.bottom,
+            paddingTop: 15,
+            flexDirection: 'row',
+            ...CENTER,
+          }}>
+          {state.routes.map((route, index) => {
+            const {options} = descriptors[route.key];
+            const label =
+              options.tabBarLabel !== undefined
+                ? options.tabBarLabel
+                : options.title !== undefined
+                ? options.title
+                : route.name === 'ContactsPageInit'
+                ? 'Contacts'
+                : route.name;
 
-          const isFocused = state.index === index;
+            const isFocused = state.index === index;
 
-          const onPress = () => {
-            const event = navigation.emit({
-              type: 'tabPress',
-              target: route.key,
-              canPreventDefault: true,
-            });
+            const onPress = () => {
+              const event = navigation.emit({
+                type: 'tabPress',
+                target: route.key,
+                canPreventDefault: true,
+              });
 
-            if (!isFocused && !event.defaultPrevented) {
-              navigation.navigate(route.name, route.params);
-            }
-          };
+              if (!isFocused && !event.defaultPrevented) {
+                navigation.navigate(route.name, route.params);
+              }
+            };
 
-          return (
-            <TouchableOpacity
-              key={index}
-              accessibilityRole="button"
-              accessibilityState={isFocused ? {selected: true} : {}}
-              accessibilityLabel={options.tabBarAccessibilityLabel}
-              testID={options.tabBarTestID}
-              onPress={onPress}
-              activeOpacity={1}
-              style={{flex: 1, alignItems: 'center'}}>
-              <View
-                style={{
-                  width: 30,
-                  height: 30,
-                  alignItems: 'center',
-                  justifyContent: 'center',
-                  // backgroundColor: isFocused
-                  //   ? COLORS.lightModeBackground
-                  //   : 'transparent',
-                  // borderRadius: 15,
-                }}>
-                {/* {label === 'Contacts' ? (
-                  <Icon
-                    name={'mail'}
-                    width={35}
-                    height={35}
-                    isActive={isFocused}
-                  />
-                ) : label === 'Home' ? (
-                  <Icon
-                    width={35}
-                    height={35}
-                    name={'wallet'}
-                    isActive={isFocused}
-                  />
-                ) : (
-                  <Icon
-                    name={'apps'}
-                    width={35}
-                    height={35}
-                    isActive={isFocused}
-                  />
-                )} */}
-                {/* <Icon name={'mail'} isActive={true} /> */}
-                <Image
+            return (
+              <TouchableOpacity
+                key={index}
+                accessibilityRole="button"
+                accessibilityState={isFocused ? {selected: true} : {}}
+                accessibilityLabel={options.tabBarAccessibilityLabel}
+                testID={options.tabBarTestID}
+                onPress={onPress}
+                activeOpacity={1}
+                style={{flex: 1, alignItems: 'center'}}>
+                <View
                   style={{
                     width: 30,
                     height: 30,
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                  }}>
+                  <Image
+                    style={{
+                      width: 30,
+                      height: 30,
+                    }}
+                    source={
+                      label === 'Contacts'
+                        ? isFocused
+                          ? ICONS.contactsIconBlueSelected
+                          : ICONS.contactsIconBlue
+                        : label === 'Home'
+                        ? isFocused
+                          ? ICONS.walletBlueIcon
+                          : ICONS.adminHomeWallet
+                        : isFocused
+                        ? ICONS.appstoreFilled
+                        : ICONS.appstore
+                    }
+                  />
+
+                  {label === 'Contacts' &&
+                    hasUnlookedTransactions.length > 0 &&
+                    !isFocused && (
+                      <View
+                        style={{
+                          position: 'absolute',
+                          top: 0,
+                          right: -2.5,
+                          width: 10,
+                          height: 10,
+                          borderRadius: 10,
+                          backgroundColor: COLORS.primary,
+                        }}></View>
+                    )}
+                </View>
+                <ThemeText
+                  styles={{
+                    fontSize: SIZES.small,
+                    marginTop: 2,
+                    fontWeight: 500,
                   }}
-                  source={
-                    label === 'Contacts'
-                      ? isFocused
-                        ? ICONS.contactsSelected
-                        : ICONS.contacts
-                      : label === 'Home'
-                      ? isFocused
-                        ? ICONS.walletBlueIcon
-                        : ICONS.adminHomeWallet
-                      : isFocused
-                      ? ICONS.appstoreFilled
-                      : ICONS.appstore
+                  content={
+                    label === 'Home'
+                      ? 'Wallet'
+                      : label === 'App Store'
+                      ? 'Store'
+                      : label
                   }
                 />
-                {label === 'Contacts' &&
-                  hasUnlookedTransactions.length > 0 &&
-                  !isFocused && (
-                    <View
-                      style={{
-                        position: 'absolute',
-                        top: 0,
-                        right: -2.5,
-                        width: 10,
-                        height: 10,
-                        borderRadius: 10,
-                        backgroundColor: COLORS.primary,
-                      }}></View>
-                  )}
-              </View>
-              {/* <ThemeText
-                styles={{fontSize: SIZES.small, marginTop: 2}}
-                content={
-                  label === 'Home'
-                    ? 'Wallet'
-                    : label === 'App Store'
-                    ? 'Store'
-                    : label
-                }
-              /> */}
-              {/* <Text
-                style={{
-                  color: theme ? COLORS.darkModeText : COLORS.lightModeText,
-                  fontFamily: FONT.Title_Regular,
-                  fontSize: SIZES.small,
-                }}>
-                {label === 'Home'
-                  ? 'Wallet'
-                  : label === 'App Store'
-                  ? 'Store'
-                  : label}
-              </Text> */}
-            </TouchableOpacity>
-          );
-        })}
+              </TouchableOpacity>
+            );
+          })}
+        </View>
       </View>
     </View>
   );
