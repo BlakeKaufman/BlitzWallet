c71a0a6a4d9dadfe5ad685cc013569267ba61d0d
Blake Kaufman <blakekaufman9@gmail.com>
2024-08-23 22:25:00 -0400

    fixed copy to clipbard with chatGPT

--- app/functions/copyToClipboard.js
+++ app/functions/copyToClipboard.js
@@ -1,13 +1,15 @@
 import * as Clipboard from 'expo-clipboard';
 
-export default async function copyToClipboard(data, navigate) {
+export default async function copyToClipboard(data, navigate, page) {
   try {
     await Clipboard.setStringAsync(data);
+    if (page === 'ChatGPT') return;
     navigate.navigate('ClipboardCopyPopup', {didCopy: true});
     return;
 
     // Alert.alert('Text Copied to Clipboard');
   } catch (err) {
+    if (page === 'ChatGPT') return;
     navigate.navigate('ClipboardCopyPopup', {didCopy: false});
     // Alert.alert('ERROR WITH COPYING');
   }
