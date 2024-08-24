db2057884d54709f4f70eb1f0bc2067d34b796d4
Blake Kaufman <blakekaufman9@gmail.com>
2024-08-24 17:37:53 -0400

    added more search begining search options

---
+++ app/components/admin/homeComponents/apps/chatGPT/contants/searchCardContent.js
@@ -0,0 +1,390 @@
+export const searchQueries = [
+  {
+    topLine: 'How do I start learning React Native?',
+    bottomLine: 'like I have never coded before',
+    fullString:
+      'How do I start learning React Native? like I have never coded before',
+  },
+  {
+    topLine: 'What is the difference between Web2 and Web3?',
+    bottomLine: "like I'm a teenager",
+    fullString:
+      "What is the difference between Web2 and Web3? like I'm a teenager",
+  },
+  {
+    topLine: 'How does the stock market work?',
+    bottomLine: 'like I am 12',
+    fullString: 'How does the stock market work? like I am 12',
+  },
+  {
+    topLine: 'What is quantum computing?',
+    bottomLine: "like I'm 10 years old",
+    fullString: "What is quantum computing? like I'm 10 years old",
+  },
+  {
+    topLine: 'Explain climate change',
+    bottomLine: 'like I’m 8',
+    fullString: 'Explain climate change like I’m 8',
+  },
+  {
+    topLine: 'What is artificial intelligence?',
+    bottomLine: "like I'm a high schooler",
+    fullString: "What is artificial intelligence? like I'm a high schooler",
+  },
+  {
+    topLine: 'How do credit cards work?',
+    bottomLine: 'like I am 15',
+    fullString: 'How do credit cards work? like I am 15',
+  },
+  {
+    topLine: 'What is blockchain technology?',
+    bottomLine: 'like I am in middle school',
+    fullString: 'What is blockchain technology? like I am in middle school',
+  },
+  {
+    topLine: 'How does GPS work?',
+    bottomLine: 'like I’m 7 years old',
+    fullString: 'How does GPS work? like I’m 7 years old',
+  },
+  {
+    topLine: 'Explain how airplanes fly',
+    bottomLine: "like I'm 9",
+    fullString: "Explain how airplanes fly like I'm 9",
+  },
+  {
+    topLine: 'What is a meme?',
+    bottomLine: 'like I’m a grandparent',
+    fullString: 'What is a meme? like I’m a grandparent',
+  },
+  {
+    topLine: 'How do wireless networks work?',
+    bottomLine: "like I'm a beginner",
+    fullString: "How do wireless networks work? like I'm a beginner",
+  },
+  {
+    topLine: 'What is cryptocurrency?',
+    bottomLine: 'like I am a teenager',
+    fullString: 'What is cryptocurrency? like I am a teenager',
+  },
+  {
+    topLine: 'How does the internet work?',
+    bottomLine: 'like I am 6',
+    fullString: 'How does the internet work? like I am 6',
+  },
+  {
+    topLine: 'What is an API?',
+    bottomLine: 'like I am new to programming',
+    fullString: 'What is an API? like I am new to programming',
+  },
+  {
+    topLine: 'Explain photosynthesis',
+    bottomLine: "like I'm in 5th grade",
+    fullString: "Explain photosynthesis like I'm in 5th grade",
+  },
+  {
+    topLine: 'How do electric cars work?',
+    bottomLine: 'like I’m 14',
+    fullString: 'How do electric cars work? like I’m 14',
+  },
+  {
+    topLine: 'What is a smart contract?',
+    bottomLine: 'like I am in high school',
+    fullString: 'What is a smart contract? like I am in high school',
+  },
+  {
+    topLine: 'How does space travel work?',
+    bottomLine: 'like I’m 13',
+    fullString: 'How does space travel work? like I’m 13',
+  },
+  {
+    topLine: 'Explain NFTs',
+    bottomLine: 'like I am 15',
+    fullString: 'Explain NFTs like I am 15',
+  },
+  {
+    topLine: 'What is augmented reality?',
+    bottomLine: "like I'm 10 years old",
+    fullString: "What is augmented reality? like I'm 10 years old",
+  },
+  {
+    topLine: 'How does a 3D printer work?',
+    bottomLine: 'like I’m in middle school',
+    fullString: 'How does a 3D printer work? like I’m in middle school',
+  },
+  {
+    topLine: 'Explain machine learning',
+    bottomLine: "like I'm in high school",
+    fullString: "Explain machine learning like I'm in high school",
+  },
+  {
+    topLine: 'How do solar panels work?',
+    bottomLine: 'like I am 12',
+    fullString: 'How do solar panels work? like I am 12',
+  },
+  {
+    topLine: 'What is virtual reality?',
+    bottomLine: 'like I’m a beginner',
+    fullString: 'What is virtual reality? like I’m a beginner',
+  },
+  {
+    topLine: 'How do vaccines work?',
+    bottomLine: 'like I am 10',
+    fullString: 'How do vaccines work? like I am 10',
+  },
+  {
+    topLine: 'Explain gravity',
+    bottomLine: 'like I’m 5 years old',
+    fullString: 'Explain gravity like I’m 5 years old',
+  },
+  {
+    topLine: 'What is a decentralized application (dApp)?',
+    bottomLine: 'like I am new to blockchain',
+    fullString:
+      'What is a decentralized application (dApp)? like I am new to blockchain',
+  },
+  {
+    topLine: 'How do microwaves cook food?',
+    bottomLine: 'like I am 8',
+    fullString: 'How do microwaves cook food? like I am 8',
+  },
+  {
+    topLine: 'Explain the concept of time zones',
+    bottomLine: "like I'm in elementary school",
+    fullString:
+      "Explain the concept of time zones like I'm in elementary school",
+  },
+
+  {
+    topLine: 'How does a computer process data?',
+    bottomLine: "like I'm a beginner",
+    fullString: "How does a computer process data? like I'm a beginner",
+  },
+  {
+    topLine: 'What is cloud computing?',
+    bottomLine: 'like I am in middle school',
+    fullString: 'What is cloud computing? like I am in middle school',
+  },
+  {
+    topLine: 'Explain the concept of AI ethics',
+    bottomLine: "like I'm a high schooler",
+    fullString: "Explain the concept of AI ethics like I'm a high schooler",
+  },
+  {
+    topLine: 'How do satellites stay in orbit?',
+    bottomLine: 'like I am 11',
+    fullString: 'How do satellites stay in orbit? like I am 11',
+  },
+  {
+    topLine: 'What is cybersecurity?',
+    bottomLine: 'like I’m a teenager',
+    fullString: 'What is cybersecurity? like I’m a teenager',
+  },
+  {
+    topLine: 'Explain the theory of evolution',
+    bottomLine: 'like I’m in 8th grade',
+    fullString: 'Explain the theory of evolution like I’m in 8th grade',
+  },
+  {
+    topLine: 'What is the purpose of the blockchain?',
+    bottomLine: 'like I am in high school',
+    fullString:
+      'What is the purpose of the blockchain? like I am in high school',
+  },
+  {
+    topLine: 'How does Wi-Fi work?',
+    bottomLine: 'like I am 9',
+    fullString: 'How does Wi-Fi work? like I am 9',
+  },
+  {
+    topLine: 'What is a search engine?',
+    bottomLine: "like I'm 10 years old",
+    fullString: "What is a search engine? like I'm 10 years old",
+  },
+  {
+    topLine: 'How do electric motors work?',
+    bottomLine: "like I'm in 7th grade",
+    fullString: "How do electric motors work? like I'm in 7th grade",
+  },
+  {
+    topLine: 'Explain the concept of relativity',
+    bottomLine: 'like I am 14',
+    fullString: 'Explain the concept of relativity like I am 14',
+  },
+  {
+    topLine: 'What is the Internet of Things (IoT)?',
+    bottomLine: 'like I am new to technology',
+    fullString:
+      'What is the Internet of Things (IoT)? like I am new to technology',
+  },
+  {
+    topLine: 'How do smartphones work?',
+    bottomLine: "like I'm in middle school",
+    fullString: "How do smartphones work? like I'm in middle school",
+  },
+  {
+    topLine: 'Explain the concept of renewable energy',
+    bottomLine: 'like I am in 6th grade',
+    fullString:
+      'Explain the concept of renewable energy like I am in 6th grade',
+  },
+  {
+    topLine: 'What is virtual currency?',
+    bottomLine: 'like I’m a teenager',
+    fullString: 'What is virtual currency? like I’m a teenager',
+  },
+  {
+    topLine: 'How do smart homes work?',
+    bottomLine: 'like I am 12',
+    fullString: 'How do smart homes work? like I am 12',
+  },
+  {
+    topLine: 'What is deep learning?',
+    bottomLine: 'like I am new to AI',
+    fullString: 'What is deep learning? like I am new to AI',
+  },
+  {
+    topLine: 'How does online shopping work?',
+    bottomLine: 'like I’m a beginner',
+    fullString: 'How does online shopping work? like I’m a beginner',
+  },
+  {
+    topLine: 'Explain the concept of data privacy',
+    bottomLine: 'like I’m a teenager',
+    fullString: 'Explain the concept of data privacy like I’m a teenager',
+  },
+  {
+    topLine: 'What is virtual reality gaming?',
+    bottomLine: 'like I am 14',
+    fullString: 'What is virtual reality gaming? like I am 14',
+  },
+  {
+    topLine: 'How do self-driving cars work?',
+    bottomLine: 'like I’m in high school',
+    fullString: 'How do self-driving cars work? like I’m in high school',
+  },
+  {
+    topLine: 'What is genetic engineering?',
+    bottomLine: 'like I am 13',
+    fullString: 'What is genetic engineering? like I am 13',
+  },
+  {
+    topLine: 'Explain the concept of smart cities',
+    bottomLine: 'like I am new to technology',
+    fullString:
+      'Explain the concept of smart cities like I am new to technology',
+  },
+  {
+    topLine: 'How do drones work?',
+    bottomLine: 'like I am 11',
+    fullString: 'How do drones work? like I am 11',
+  },
+  {
+    topLine: 'What is a digital wallet?',
+    bottomLine: "like I'm a teenager",
+    fullString: "What is a digital wallet? like I'm a teenager",
+  },
+  {
+    topLine: 'How does digital art work?',
+    bottomLine: 'like I’m in middle school',
+    fullString: 'How does digital art work? like I’m in middle school',
+  },
+  {
+    topLine: 'Explain the concept of 5G technology',
+    bottomLine: 'like I am new to tech',
+    fullString: 'Explain the concept of 5G technology like I am new to tech',
+  },
+  {
+    topLine: 'What is an autonomous vehicle?',
+    bottomLine: 'like I am 15',
+    fullString: 'What is an autonomous vehicle? like I am 15',
+  },
+  {
+    topLine: 'How do video games work?',
+    bottomLine: 'like I am 10',
+    fullString: 'How do video games work? like I am 10',
+  },
+  {
+    topLine: 'Explain the concept of smart contracts',
+    bottomLine: 'like I’m in high school',
+    fullString:
+      'Explain the concept of smart contracts like I’m in high school',
+  },
+  {
+    topLine: 'What is a smart watch?',
+    bottomLine: 'like I am in middle school',
+    fullString: 'What is a smart watch? like I am in middle school',
+  },
+  {
+    topLine: 'How does a touchscreen work?',
+    bottomLine: "like I'm 12 years old",
+    fullString: "How does a touchscreen work? like I'm 12 years old",
+  },
+  {
+    topLine: 'Explain the concept of machine translation',
+    bottomLine: 'like I’m a beginner',
+    fullString:
+      'Explain the concept of machine translation like I’m a beginner',
+  },
+  {
+    topLine: 'What is a data center?',
+    bottomLine: 'like I am new to technology',
+    fullString: 'What is a data center? like I am new to technology',
+  },
+  {
+    topLine: 'How does blockchain mining work?',
+    bottomLine: 'like I’m a teenager',
+    fullString: 'How does blockchain mining work? like I’m a teenager',
+  },
+  {
+    topLine: 'What is a microchip?',
+    bottomLine: "like I'm in high school",
+    fullString: "What is a microchip? like I'm in high school",
+  },
+  {
+    topLine: 'Explain the concept of cloud storage',
+    bottomLine: 'like I am in middle school',
+    fullString:
+      'Explain the concept of cloud storage like I am in middle school',
+  },
+  {
+    topLine: 'How do electric scooters work?',
+    bottomLine: 'like I’m 13',
+    fullString: 'How do electric scooters work? like I’m 13',
+  },
+  {
+    topLine: 'What is an algorithm?',
+    bottomLine: "like I'm a beginner",
+    fullString: "What is an algorithm? like I'm a beginner",
+  },
+  {
+    topLine: 'Explain the concept of virtual meetings',
+    bottomLine: 'like I am new to technology',
+    fullString:
+      'Explain the concept of virtual meetings like I am new to technology',
+  },
+  {
+    topLine: 'How do electric bikes work?',
+    bottomLine: 'like I am 14',
+    fullString: 'How do electric bikes work? like I am 14',
+  },
+  {
+    topLine: 'What is a quantum computer?',
+    bottomLine: 'like I’m in high school',
+    fullString: 'What is a quantum computer? like I’m in high school',
+  },
+  {
+    topLine: 'Explain the concept of cybersecurity',
+    bottomLine: 'like I am a teenager',
+    fullString: 'Explain the concept of cybersecurity like I am a teenager',
+  },
+  {
+    topLine: 'What is augmented reality?',
+    bottomLine: 'like I am 12',
+    fullString: 'What is augmented reality? like I am 12',
+  },
+  {
+    topLine: 'How do streaming services work?',
+    bottomLine: 'like I’m in middle school',
+    fullString: 'How do streaming services work? like I’m in middle school',
+  },
+];
--- app/components/admin/homeComponents/apps/chatGPT/exampleSearchCards.js
+++ app/components/admin/homeComponents/apps/chatGPT/exampleSearchCards.js
@@ -2,21 +2,19 @@ import {ScrollView, StyleSheet, TouchableOpacity, View} from 'react-native';
 import {ThemeText} from '../../../../../functions/CustomElements';
 import {COLORS} from '../../../../../constants';
 import {useGlobalContextProvider} from '../../../../../../context-store/context';
+import {shuffleArray} from '../../../../../functions';
+import {searchQueries} from './contants/searchCardContent';
 
-export default function ExampleGPTSearchCard({
-  submitChaMessage,
-  setUserChatText,
-}) {
+export default function ExampleGPTSearchCard({submitChaMessage}) {
   const {theme} = useGlobalContextProvider();
-  return (
-    <View style={styles.container}>
-      <ScrollView
-        keyboardShouldPersistTaps={'always'}
-        horizontal
-        showsHorizontalScrollIndicator={false}>
+  const cardElements = shuffleArray(searchQueries)
+    .slice(0, 6)
+    .map((item, index) => {
+      return (
         <TouchableOpacity
+          key={index}
           onPress={() => {
-            submitChaMessage('Explain Bitcoin to me like I am 5');
+            submitChaMessage(item.fullString);
           }}
           style={[
             styles.contentContainer,
@@ -28,71 +26,18 @@ export default function ExampleGPTSearchCard({
               marginLeft: 0,
             },
           ]}>
-          <ThemeText
-            styles={styles.headerText}
-            content={'Explain Bitcoin to me'}
-          />
-          <ThemeText styles={styles.subHeaderText} content={'like I am 5'} />
+          <ThemeText styles={styles.headerText} content={item.topLine} />
+          <ThemeText styles={styles.subHeaderText} content={item.bottomLine} />
         </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => {
-            submitChaMessage('Write a thank-you note to my interviwer');
-          }}
-          style={[
-            styles.contentContainer,
-            {
-              backgroundColor: theme
-                ? COLORS.darkModeBackgroundOffset
-                : COLORS.lightModeBackgroundOffset,
-            },
-          ]}>
-          <ThemeText
-            styles={styles.headerText}
-            content={'Write a thank-you note'}
-          />
-          <ThemeText
-            styles={styles.subHeaderText}
-            content={'to my interviwer'}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => {
-            submitChaMessage(
-              'Create a morning routinie to boost my productivity',
-            );
-          }}
-          style={[
-            styles.contentContainer,
-            {
-              backgroundColor: theme
-                ? COLORS.darkModeBackgroundOffset
-                : COLORS.lightModeBackgroundOffset,
-            },
-          ]}>
-          <ThemeText
-            styles={styles.headerText}
-            content={'Create a morning routinie'}
-          />
-          <ThemeText
-            styles={styles.subHeaderText}
-            content={'to boost my productivity'}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity
-          onPress={() => {
-            submitChaMessage('Test my knowledge on ancient civilizations');
-          }}
-          style={[
-            styles.contentContainer,
-            {
-              backgroundColor: theme
-                ? COLORS.darkModeBackgroundOffset
-                : COLORS.lightModeBackgroundOffset,
-            },
-          ]}>
-          <ThemeText styles={styles.headerText} content={'Test my knowledge'} />
-          <ThemeText styles={styles.sub} content={'on ancient civilizations'} />
-        </TouchableOpacity>
+      );
+    });
+  return (
+    <View style={styles.container}>
+      <ScrollView
+        keyboardShouldPersistTaps={'always'}
+        horizontal
+        showsHorizontalScrollIndicator={false}>
+        {cardElements}
       </ScrollView>
     </View>
   );
