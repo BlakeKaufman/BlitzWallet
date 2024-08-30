c263681cc22a233026c757638b20e1cc7c419c0d
Blake Kaufman <blakekaufman9@gmail.com>
2024-08-30 12:12:45 -0400

    fixed bug

--- app/components/admin/homeComponents/homeLightning.js
+++ app/components/admin/homeComponents/homeLightning.js
@@ -46,6 +46,8 @@ export default function HomeLightning({tabNavigation}) {
     nodeTransactions,
     liquidTransactions,
     masterFailedTransactions,
+    showAmount,
+    theme,
   ]);
 
   return (
