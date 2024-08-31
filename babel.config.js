5a5e603c9d5c7a6020b767c63c6046cf79943411
Blake Kaufman <blakekaufman9@gmail.com>
2024-08-31 19:31:57 -0400

    remove console.log messages in production

--- babel.config.js
+++ babel.config.js
@@ -1,23 +1,28 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    ['module:react-native-dotenv'],
-    ['react-native-reanimated/plugin'],
-    // [
-    //   'transform-inline-environment-variables',
-    //   {
-    //     include: ['GL_CUSTOM_NOBODY_CERT', 'GL_CUSTOM_NOBODY_KEY', 'API_KEY'],
-    //   },
-    // ],
-    [
-      'module-resolver',
-      {
-        alias: {
-          crypto: 'react-native-quick-crypto',
-          stream: 'stream-browserify',
-          buffer: '@craftzdog/react-native-buffer',
+module.exports = api => {
+  const isProduction = api.env('production');
+
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      ['module:react-native-dotenv'],
+      ['react-native-reanimated/plugin'],
+      isProduction && ['transform-remove-console'],
+      // [
+      //   'transform-inline-environment-variables',
+      //   {
+      //     include: ['GL_CUSTOM_NOBODY_CERT', 'GL_CUSTOM_NOBODY_KEY', 'API_KEY'],
+      //   },
+      // ],
+      [
+        'module-resolver',
+        {
+          alias: {
+            crypto: 'react-native-quick-crypto',
+            stream: 'stream-browserify',
+            buffer: '@craftzdog/react-native-buffer',
+          },
         },
-      },
-    ],
-  ],
+      ],
+    ].filter(Boolean),
+  };
 };
