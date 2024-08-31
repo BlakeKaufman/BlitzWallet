5276eb483a99adfad28fa218c96fb673e4171e63
Blake Kaufman <blakekaufman9@gmail.com>
2024-08-31 18:29:22 -0400

    added default option to sat text display

--- app/functions/CustomElements/satTextDisplay.js
+++ app/functions/CustomElements/satTextDisplay.js
@@ -57,7 +57,7 @@ export default function FormattedSatText({
             (localBalanceDenomination === 'hidden' && neverHideBalance))
             ? ''
             : localBalanceDenomination === 'fiat'
-            ? ` ${nodeInformation.fiatStats.coin}`
+            ? ` ${nodeInformation.fiatStats.coin || 'USD'}`
             : localBalanceDenomination === 'hidden' && !neverHideBalance
             ? '* * * * *'
             : ' sats'
