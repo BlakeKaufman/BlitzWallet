b2a29ded4508b381b3de07772b3fa79de4523a78
Blake Kaufman <blakekaufman9@gmail.com>
2024-09-04 20:42:44 -0400

    configured notifications for ios

--- ios/BlitzWallet/AppDelegate.mm
+++ ios/BlitzWallet/AppDelegate.mm
@@ -1,6 +1,7 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import "RNNotifications.h"
 
 @implementation AppDelegate
 
@@ -12,6 +13,8 @@
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
 
+  [RNNotifications startMonitorNotifications]; // -> notifications
+
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
@@ -22,6 +25,14 @@
 #else
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
 #endif
+}
+
+- (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
+  [RNNotifications didRegisterForRemoteNotificationsWithDeviceToken:deviceToken];
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+  [RNNotifications didFailToRegisterForRemoteNotificationsWithError:error];
 }
 
 @end
