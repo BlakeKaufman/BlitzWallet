05803134a1154bb6d31e2b199982de67cbfccccd
Blake Kaufman <blakekaufman9@gmail.com>
2024-09-05 20:51:40 -0400

    fixed background notification on ios

--- ios/BlitzWallet/AppDelegate.mm
+++ ios/BlitzWallet/AppDelegate.mm
@@ -35,4 +35,7 @@
   [RNNotifications didFailToRegisterForRemoteNotificationsWithError:error];
 }
 
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo fetchCompletionHandler:(void (^)(UIBackgroundFetchResult result))completionHandler {
+  [RNNotifications didReceiveBackgroundNotification:userInfo withCompletionHandler:completionHandler];
+}
 @end
