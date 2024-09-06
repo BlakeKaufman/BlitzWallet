64d44afc14e8e3bb9905139c4271deb3a723480c
Blake Kaufman <blakekaufman9@gmail.com>
2024-09-06 07:22:00 -0400

    added check to see if swap was already claimed

--- context-store/notificationManager.js
+++ context-store/notificationManager.js
@@ -5,168 +5,190 @@ import {getBoltzWsUrl} from '../app/functions/boltz/boltzEndpoitns';
 import WebView from 'react-native-webview';
 import handleReverseClaimWSS from '../app/functions/boltz/handle-reverse-claim-wss';
 import handleWebviewClaimMessage from '../app/functions/boltz/handle-webview-claim-message';
-import {getLocalStorageItem, setLocalStorageItem} from '../app/functions';
+import {
+  getLocalStorageItem,
+  retrieveData,
+  setLocalStorageItem,
+} from '../app/functions';
 
 import {useGlobalContextProvider} from './context';
 import {addDataToCollection} from '../db';
 
 const PushNotificationManager = ({children}) => {
-  const isInitialRender = useRef(true);
+  // const isInitialRender = useRef(true);
   const webViewRef = useRef(null);
   const [webViewArgs, setWebViewArgs] = useState({
     page: null,
     function: null,
   });
-  const {didGetToHomepage} = useGlobalContextProvider();
+  // const {didGetToHomepage} = useGlobalContextProvider();
+
+  // const [isClaimingSwapFromNotification, setIsClaimingSwapFromNotification] =
+  // useState(false);
 
   useEffect(() => {
-    if (Platform.OS === 'ios') Notifications.ios.setBadgeCount(0);
+    async function initNotification() {
+      let receivedSwaps = [];
+      // const mnemoinc = await retrieveData('mnemonic');
 
-    const registerDevice = async () => {
-      Notifications.events().registerRemoteNotificationsRegistered(
-        async event => {
-          const deviceToken = event.deviceToken;
-          console.log('Device Token Received', deviceToken);
+      if (Platform.OS === 'ios') Notifications.ios.setBadgeCount(0);
+      const registerDevice = async () => {
+        if (Platform.OS === 'android') {
+          const test = Notifications.android.registerRemoteNotifications();
+          console.log(test, 'TTT');
 
-          const savedDeviceToken = JSON.parse(
-            await getLocalStorageItem('pushToken'),
-          );
-          const encriptedText = savedDeviceToken.encriptedText;
-          console.log(savedDeviceToken.encriptedText, 'SAVED DEVIE TOKEN');
+          return;
+        }
+        Notifications.events().registerRemoteNotificationsRegistered(
+          async event => {
+            const deviceToken = event.deviceToken;
+            console.log('Device Token Received', deviceToken);
 
-          if (!savedDeviceToken) {
+            const savedDeviceToken = JSON.parse(
+              await getLocalStorageItem('pushToken'),
+            );
+            const encriptedText = savedDeviceToken.encriptedText;
+            console.log(savedDeviceToken.encriptedText, 'SAVED DEVIE TOKEN');
+
+            if (!savedDeviceToken) {
+              savePushNotificationToDatabase(deviceToken);
+              return;
+            }
+
+            const decriptedToken = await (
+              await fetch(
+                'https://blitz-wallet.com/.netlify/functions/decriptMessage',
+                {
+                  method: 'POST', // Specify the HTTP method
+                  headers: {
+                    'Content-Type': 'application/json', // Set the content type to JSON
+                  },
+                  body: JSON.stringify({
+                    encriptedText, // The text property in the body
+                  }),
+                },
+              )
+            ).json();
+
+            console.log(
+              decriptedToken.decryptedText === deviceToken,
+              'ARE THEY EQUAll',
+            );
+            if (decriptedToken.decryptedText === deviceToken) {
+              console.log('RUNNING IN HERE');
+              return;
+            }
+            console.log('RUNNING AFTER');
             savePushNotificationToDatabase(deviceToken);
-            return;
-          }
+          },
+        );
+        Notifications.events().registerRemoteNotificationsRegistrationFailed(
+          event => {
+            console.error('event-err', event);
+          },
+        );
 
-          const decriptedToken = await (
+        Notifications.registerRemoteNotifications();
+      };
+
+      const savePushNotificationToDatabase = async pushKey => {
+        try {
+          const em = await (
             await fetch(
-              'https://blitz-wallet.com/.netlify/functions/decriptMessage',
+              'https://blitz-wallet.com/.netlify/functions/encriptMessage',
               {
                 method: 'POST', // Specify the HTTP method
                 headers: {
                   'Content-Type': 'application/json', // Set the content type to JSON
                 },
                 body: JSON.stringify({
-                  encriptedText, // The text property in the body
+                  text: pushKey, // The text property in the body
                 }),
               },
             )
           ).json();
-
-          console.log(
-            decriptedToken.decryptedText === deviceToken,
-            'ARE THEY EQUAll',
-          );
-          if (decriptedToken.decryptedText === deviceToken) {
-            console.log('RUNNING IN HERE');
-            return;
-          }
-          console.log('RUNNING AFTER');
-          savePushNotificationToDatabase(deviceToken);
-        },
-      );
-      Notifications.events().registerRemoteNotificationsRegistrationFailed(
-        event => {
-          console.error('event-err', event);
-        },
-      );
-
-      Notifications.registerRemoteNotifications();
-    };
-
-    const savePushNotificationToDatabase = async pushKey => {
-      try {
-        const em = await (
-          await fetch(
-            'https://blitz-wallet.com/.netlify/functions/encriptMessage',
+          setLocalStorageItem('pushToken', JSON.stringify(em));
+          addDataToCollection(
             {
-              method: 'POST', // Specify the HTTP method
-              headers: {
-                'Content-Type': 'application/json', // Set the content type to JSON
-              },
-              body: JSON.stringify({
-                text: pushKey, // The text property in the body
-              }),
+              pushNotifications: {platform: Platform.OS, key: em},
             },
-          )
-        ).json();
-        setLocalStorageItem('pushToken', JSON.stringify(em));
-        addDataToCollection(
-          {
-            pushNotifications: {platform: Platform.OS, key: em},
-          },
-          'blitzWalletUsers',
-        );
-      } catch (err) {
-        console.log(`Saving push notification to database error:`, err);
-      }
-    };
-
-    const registerNotificationEvents = () => {
-      Notifications.events().registerNotificationReceivedForeground(
-        (notification, completion) => {
-          console.log('Notification Received - Foreground', notification);
-
-          const webSocket = new WebSocket(
-            `${getBoltzWsUrl(process.env.BOLTZ_ENVIRONMENT)}`,
+            'blitzWalletUsers',
           );
+        } catch (err) {
+          console.log(`Saving push notification to database error:`, err);
+        }
+      };
 
-          if (Platform.OS === 'ios') {
-            const {
-              payload: {privateKey, preimage, swapInfo, liquidAddress},
-            } = notification;
-
-            console.log(privateKey);
-            console.log(preimage);
-            console.log(swapInfo);
-            console.log(liquidAddress);
-
-            setWebViewArgs({page: 'notifications'});
-
-            handleReverseClaimWSS({
-              ref: webViewRef,
-              webSocket,
-              liquidAddress,
-              swapInfo,
-              preimage,
-              privateKey: privateKey,
-            });
-
-            // Calling completion on iOS with `alert: true` will present the native iOS inApp notification.
+      const registerNotificationEvents = () => {
+        Notifications.events().registerNotificationReceivedForeground(
+          (notification, completion) => {
+            console.log('Notification Received - Foreground', notification);
+            handleSwap(notification, receivedSwaps);
             completion({alert: true, sound: false, badge: false});
-          }
-        },
-      );
+          },
+        );
 
-      if (!isInitialRender.current) return;
-      isInitialRender.current = false;
-      Notifications.events().registerNotificationOpened(
-        (notification, completion) => {
-          console.log('Notification opened by device user', notification);
-          completion();
-        },
-      );
+        Notifications.events().registerNotificationOpened(
+          (notification, completion) => {
+            console.log('Notification opened by device user', notification);
+            if (notification) handleSwap(notification, receivedSwaps);
+            completion();
+          },
+        );
 
-      Notifications.events().registerNotificationReceivedBackground(
-        (notification, completion) => {
-          console.log('Notification Received - Background', notification);
-          completion({alert: true, sound: true, badge: false});
-        },
-      );
+        Notifications.events().registerNotificationReceivedBackground(
+          (notification, completion) => {
+            console.log('Notification Received - Background', notification);
+            handleSwap(notification, receivedSwaps);
+            completion({alert: true, sound: true, badge: false});
+          },
+        );
 
-      Notifications.getInitialNotification()
-        .then(notification => {
-          console.log('Initial notification was:', notification || 'N/A');
-        })
-        .catch(err => console.error('getInitialNotification() failed', err));
-    };
+        Notifications.getInitialNotification()
+          .then(notification => {
+            console.log('Initial notification was:', notification || 'N/A');
+            if (notification) handleSwap(notification, receivedSwaps);
+          })
+          .catch(err => console.error('getInitialNotification() failed', err));
+      };
 
-    if (!didGetToHomepage) return;
+      registerDevice();
+      registerNotificationEvents();
+    }
+    initNotification();
+  }, []);
 
-    registerDevice();
-    registerNotificationEvents();
-  }, [didGetToHomepage]);
+  const handleSwap = (notification, receivedSwaps) => {
+    const webSocket = new WebSocket(
+      `${getBoltzWsUrl(process.env.BOLTZ_ENVIRONMENT)}`,
+    );
+
+    if (Platform.OS === 'ios') {
+      const {
+        payload: {privateKey, preimage, swapInfo, liquidAddress},
+      } = notification;
+      if (receivedSwaps.find(swapId => swapId === swapId.id)) return;
+      receivedSwaps.push(swapInfo.id);
+
+      console.log(privateKey);
+      console.log(preimage);
+      console.log(swapInfo);
+      console.log(liquidAddress);
+
+      setWebViewArgs({page: 'notifications'});
+
+      handleReverseClaimWSS({
+        ref: webViewRef,
+        webSocket,
+        liquidAddress,
+        swapInfo,
+        preimage,
+        privateKey: privateKey,
+      });
+
+      // Calling completion on iOS with `alert: true` will present the native iOS inApp notification.
+    }
+  };
 
   return (
     <View style={{flex: 1}}>
