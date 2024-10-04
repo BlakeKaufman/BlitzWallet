271823c4355dea6243a35cc2825b2ad6e36fcd19
Blake Kaufman <blakekaufman9@gmail.com>
2024-10-04 13:58:00 -0400

    updated ruleset

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,7 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'react-native/no-inline-styles': 'off',
     'dot-notation': 'off',
+    curly: 'off',
+    'react-hooks/exhaustive-deps': 'off',
   },
 };
