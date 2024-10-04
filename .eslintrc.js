96897ecf12f32a7865736c5a110fd8ce362a91ae
Blake Kaufman <blakekaufman9@gmail.com>
2024-10-04 13:55:36 -0400

    updated ruleset

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react-native/no-inline-styles': 'off',
+    'dot-notation': 'off',
+  },
 };
