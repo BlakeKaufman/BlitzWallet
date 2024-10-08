9e7bbdc840eb7fe754ee807d82d3c6f47b6da23a
Blake Kaufman <blakekaufman9@gmail.com>
2024-10-08 12:18:05 -0400

    cache wallet, client, and signer state

--- app/functions/liquidWallet/index.js
+++ app/functions/liquidWallet/index.js
@@ -1,7 +1,4 @@
 import {retrieveData, storeData} from '../secureStore';
-import {useEffect, useState} from 'react';
-// import {useGlobalContextProvider} from '../../../context-store/context';
-import * as Filesystem from 'expo-file-system';
 
 import {assetIDS} from './assetIDS';
 import {Wollet, Client, Signer, Network, TxBuilder} from 'lwk-rn';
@@ -20,6 +17,52 @@ const network =
   process.env.BOLTZ_ENVIRONMENT === 'testnet'
     ? Network.Testnet
     : Network.Mainnet;
+
+let wolletState;
+let clientState;
+let signerState;
+
+async function getWolletState() {
+  if (wolletState && clientState && signerState)
+    return {wolletState, clientState, signerState};
+
+  try {
+    const mnemonic = await generateLiquidMnemonic();
+    signerState = await new Signer().create(mnemonic, network);
+    const descriptor = await signerState.wpkhSlip77Descriptor();
+
+    wolletState = await new Wollet().create(network, descriptor, null);
+    clientState = await new Client().defaultElectrumClient(network);
+
+    return {
+      wolletState: wolletState,
+      clientState: clientState,
+      signerState: signerState,
+    };
+  } catch (err) {
+    return false;
+  }
+}
+
+async function getWalletInfo() {
+  try {
+    const mnemonic = await generateLiquidMnemonic();
+    const signer = await new Signer().create(mnemonic, network);
+    const descriptor = await signer.wpkhSlip77Descriptor();
+
+    const wollet = await new Wollet().create(network, descriptor, null);
+
+    const client = await new Client().defaultElectrumClient(network);
+    const update = await client.fullScan(wollet);
+    await wollet.applyUpdate(update);
+
+    console.log('GET WALLET INFOR');
+
+    return {network, signer, descriptor, wollet, client};
+  } catch (err) {
+    return false;
+  }
+}
 async function startGDKSession() {
   try {
     const didStart = await getWalletInfo();
@@ -95,13 +138,14 @@ async function createLiquidReceiveAddress() {
         storedLiquidAddress[0].startsWith('l')) ||
       isMoreThanADayOld(storedLiquidAddress[1])
     ) {
-      const mnemonic = await generateLiquidMnemonic();
-      const signer = await new Signer().create(mnemonic, network);
-      const descriptor = await signer.wpkhSlip77Descriptor();
-      const wollet = await new Wollet().create(network, descriptor, null);
+      const {wolletState} = await getWolletState();
+      // const mnemonic = await generateLiquidMnemonic();
+      // const signer = await new Signer().create(mnemonic, network);
+      // const descriptor = await signer.wpkhSlip77Descriptor();
+      // const wollet = await new Wollet().create(network, descriptor, null);
 
       const adressNumber = await updateLiquidReceiveAddressNumber();
-      const address = await wollet.getAddress(
+      const address = await wolletState.getAddress(
         Platform.OS === 'ios' ? String(adressNumber) : adressNumber,
       );
 
@@ -120,17 +164,18 @@ async function createLiquidReceiveAddress() {
 }
 async function getLiquidTransactions() {
   try {
-    const mnemonic = await generateLiquidMnemonic();
-    const signer = await new Signer().create(mnemonic, network);
-    const descriptor = await signer.wpkhSlip77Descriptor();
+    const {wolletState, clientState} = await getWolletState();
+    // const mnemonic = await generateLiquidMnemonic();
+    // const signer = await new Signer().create(mnemonic, network);
+    // const descriptor = await signer.wpkhSlip77Descriptor();
 
-    const wollet = await new Wollet().create(network, descriptor, null);
+    // const wollet = await new Wollet().create(network, descriptor, null);
 
-    const client = await new Client().defaultElectrumClient(network);
-    const update = await client.fullScan(wollet);
-    await wollet.applyUpdate(update);
+    // const client = await new Client().defaultElectrumClient(network);
+    const update = await clientState.fullScan(wolletState);
+    await wolletState.applyUpdate(update);
 
-    const transactions = await wollet.getTransactions();
+    const transactions = await wolletState.getTransactions();
     return transactions;
   } catch (error) {
     console.log('LIQUID TRANSACTIONS', error);
@@ -139,17 +184,18 @@ async function getLiquidTransactions() {
 }
 async function getLiquidBalance() {
   try {
-    const mnemonic = await generateLiquidMnemonic();
-    const signer = await new Signer().create(mnemonic, network);
-    const descriptor = await signer.wpkhSlip77Descriptor();
+    const {wolletState, clientState} = await getWolletState();
+    // const mnemonic = await generateLiquidMnemonic();
+    // const signer = await new Signer().create(mnemonic, network);
+    // const descriptor = await signer.wpkhSlip77Descriptor();
 
-    const wollet = await new Wollet().create(network, descriptor, null);
+    // const wollet = await new Wollet().create(network, descriptor, null);
 
-    const client = await new Client().defaultElectrumClient(network);
-    const update = await client.fullScan(wollet);
-    await wollet.applyUpdate(update);
+    // const client = await new Client().defaultElectrumClient(network);
+    const update = await clientState.fullScan(wolletState);
+    await wolletState.applyUpdate(update);
 
-    const userBalance = await wollet.getBalance();
+    const userBalance = await wolletState.getBalance();
 
     console.log(userBalance[assetIDS['L-BTC']]);
 
@@ -162,13 +208,14 @@ async function getLiquidBalance() {
 
 async function getLiquidTxFee({amountSat, address}) {
   try {
-    const mnemonic = await generateLiquidMnemonic();
-    const signer = await new Signer().create(mnemonic, network);
-    const descriptor = await signer.wpkhSlip77Descriptor();
-    const wollet = await new Wollet().create(network, descriptor, null);
-    const client = await new Client().defaultElectrumClient(network);
-    const update = await client.fullScan(wollet);
-    await wollet.applyUpdate(update);
+    const {wolletState, clientState, signerState} = await getWolletState();
+    // const mnemonic = await generateLiquidMnemonic();
+    // const signer = await new Signer().create(mnemonic, network);
+    // const descriptor = await signer.wpkhSlip77Descriptor();
+    // const wollet = await new Wollet().create(network, descriptor, null);
+    // const client = await new Client().defaultElectrumClient(network);
+    const update = await clientState.fullScan(wolletState);
+    await wolletState.applyUpdate(update);
 
     const address =
       process.env.BOLTZ_ENVIRONMENT === 'liquid'
@@ -184,10 +231,10 @@ async function getLiquidTxFee({amountSat, address}) {
 
     await builder.feeRate(fee_rate);
 
-    let pset = await builder.finish(wollet);
+    let pset = await builder.finish(wolletState);
 
-    let signed_pset = await signer.sign(pset);
-    let finalized_pset = await wollet.finalize(signed_pset);
+    let signed_pset = await signerState.sign(pset);
+    let finalized_pset = await wolletState.finalize(signed_pset);
     const tx = await finalized_pset.extractTx();
 
     return await tx.fee(assetIDS['L-BTC']);
@@ -199,18 +246,19 @@ async function getLiquidTxFee({amountSat, address}) {
 
 async function getLiquidBalanceAndTransactions() {
   try {
-    const mnemonic = await generateLiquidMnemonic();
-    const signer = await new Signer().create(mnemonic, network);
-    const descriptor = await signer.wpkhSlip77Descriptor();
+    const {wolletState, clientState} = await getWolletState();
+    // const mnemonic = await generateLiquidMnemonic();
+    // const signer = await new Signer().create(mnemonic, network);
+    // const descriptor = await signer.wpkhSlip77Descriptor();
 
-    const wollet = await new Wollet().create(network, descriptor, null);
+    // const wollet = await new Wollet().create(network, descriptor, null);
 
-    const client = await new Client().defaultElectrumClient(network);
-    const update = await client.fullScan(wollet);
-    await wollet.applyUpdate(update);
+    // const client = await new Client().defaultElectrumClient(network);
+    const update = await clientState.fullScan(wolletState);
+    await wolletState.applyUpdate(update);
 
-    const userBalance = await wollet.getBalance();
-    const transactions = await wollet.getTransactions();
+    const userBalance = await wolletState.getBalance();
+    const transactions = await wolletState.getTransactions();
 
     console.log(userBalance[assetIDS['L-BTC']]);
 
@@ -226,33 +274,34 @@ async function getLiquidBalanceAndTransactions() {
 
 async function sendLiquidTransaction(amountSat, address, doesNeedToWait) {
   try {
-    const mnemonic = await generateLiquidMnemonic();
-    const signer = await new Signer().create(mnemonic, network);
-    const descriptor = await signer.wpkhSlip77Descriptor();
-    const wollet = await new Wollet().create(network, descriptor, null);
-    const client = await new Client().defaultElectrumClient(network);
-    const update = await client.fullScan(wollet);
-    await wollet.applyUpdate(update);
+    const {wolletState, clientState, signerState} = await getWolletState();
+    // const mnemonic = await generateLiquidMnemonic();
+    // const signer = await new Signer().create(mnemonic, network);
+    // const descriptor = await signer.wpkhSlip77Descriptor();
+    // const wollet = await new Wollet().create(network, descriptor, null);
+    // const client = await new Client().defaultElectrumClient(network);
+    const update = await clientState.fullScan(wolletState);
+    await wolletState.applyUpdate(update);
 
     const fee_rate = process.env.BOLTZ_ENVIRONMENT === 'testnet' ? 12 : 100; // this is the sat/vB * 100 fee rate. Example 280 would equal a fee rate of .28 sat/vB. 100 would equal .1 sat/vB
     const builder = await new TxBuilder().create(network);
     await builder.addLbtcRecipient(address, amountSat);
     await builder.feeRate(fee_rate);
-    let pset = await builder.finish(wollet);
-    let signed_pset = await signer.sign(pset);
-    let finalized_pset = await wollet.finalize(signed_pset);
+    let pset = await builder.finish(wolletState);
+    let signed_pset = await signerState.sign(pset);
+    let finalized_pset = await wolletState.finalize(signed_pset);
     const tx = await finalized_pset.extractTx();
-    await client.broadcast(tx);
+    await clientState.broadcast(tx);
 
-    if (doesNeedToWait) {
-      return new Promise(resolve => {
-        setTimeout(() => {
-          resolve(true);
-        }, 5000); // Delay for 5 seconds
-      });
-    } else {
-      return true;
-    }
+    // if (doesNeedToWait) {
+    //   return new Promise(resolve => {
+    //     setTimeout(() => {
+    //       resolve(true);
+    //     }, 5000); // Delay for 5 seconds
+    //   });
+    // } else {
+    return true;
+    // }
   } catch (error) {
     console.log('SEND PAYMENT ERROR', error);
     return false;
@@ -307,26 +356,6 @@ export const updateLiquidWalletInformation = async ({
   );
   return true;
 };
-
-async function getWalletInfo() {
-  try {
-    const mnemonic = await generateLiquidMnemonic();
-    const signer = await new Signer().create(mnemonic, network);
-    const descriptor = await signer.wpkhSlip77Descriptor();
-
-    const wollet = await new Wollet().create(network, descriptor, null);
-
-    const client = await new Client().defaultElectrumClient(network);
-    const update = await client.fullScan(wollet);
-    await wollet.applyUpdate(update);
-
-    console.log('GET WALLET INFOR');
-
-    return {network, signer, descriptor, wollet, client};
-  } catch (err) {
-    return false;
-  }
-}
 
 async function updateLiquidReceiveAddressNumber() {
   try {
