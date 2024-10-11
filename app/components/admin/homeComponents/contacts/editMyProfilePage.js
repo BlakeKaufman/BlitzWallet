5149ccb36d9fd0369fd093a7f3265e44c7137b9a
Blake Kaufman <blakekaufman9@gmail.com>
2024-10-11 08:03:19 -0400

    fixed regex bug

--- app/components/admin/homeComponents/contacts/editMyProfilePage.js
+++ app/components/admin/homeComponents/contacts/editMyProfilePage.js
@@ -438,7 +438,7 @@ function InnerContent({isEditingMyProfile, selectedAddedContact}) {
       ) {
         navigate.goBack();
       } else {
-        if (VALID_USERNAME_REGEX.test(inputs.uniquename)) {
+        if (!VALID_USERNAME_REGEX.test(inputs.uniquename)) {
           navigate.navigate('ErrorScreen', {
             errorMessage:
               'You can only have letters and numbers in your username',
