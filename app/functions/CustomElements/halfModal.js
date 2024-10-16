717291f5eebb0da29682500eb4555d06942465ba
Blake Kaufman <blakekaufman9@gmail.com>
2024-10-16 11:51:25 -0400

    updated half modal to account for keyboard

--- app/functions/CustomElements/halfModal.js
+++ app/functions/CustomElements/halfModal.js
@@ -1,15 +1,17 @@
+import React, {useCallback, useEffect, useRef} from 'react';
 import {
   Animated,
+  Keyboard,
+  KeyboardAvoidingView,
+  Platform,
   StyleSheet,
   TouchableWithoutFeedback,
   View,
   useWindowDimensions,
 } from 'react-native';
-import {CENTER, COLORS, FONT, ICONS, SIZES} from '../../constants';
 import {useNavigation} from '@react-navigation/native';
-
+import {COLORS} from '../../constants';
 import handleBackPress from '../../hooks/handleBackPress';
-import {useCallback, useEffect, useRef} from 'react';
 import {HalfModalSendOptions} from '../../components/admin';
 import {
   ConfirmSMSPayment,
@@ -19,24 +21,29 @@ import ThemeText from './textTheme';
 import ConfirmGiftCardPurchase from '../../components/admin/homeComponents/apps/giftCards/confimPurchase';
 import ConfirmExportPayments from '../../components/admin/homeComponents/exportTransactions/exportTracker';
 import ConfirmChatGPTPage from '../../components/admin/homeComponents/apps/chatGPT/components/confirmationPage';
+import AddContactsHalfModal from '../../components/admin/homeComponents/contacts/addContactsHalfModal';
+import GetThemeColors from '../../hooks/themeColors';
 
 export default function CustomHalfModal(props) {
-  const navigate = useNavigation();
+  const navigation = useNavigation();
+  const windowDimensions = useWindowDimensions();
 
   const contentType = props?.route?.params?.wantedContent;
   const slideHeight = props?.route?.params?.sliderHight || 0.5;
+  const {backgroundColor} = GetThemeColors();
 
-  console.log(contentType);
-
-  const windowDimensions = useWindowDimensions();
+  const translateY = useRef(
+    new Animated.Value(windowDimensions.height),
+  ).current;
 
   const handleBackPressFunction = useCallback(() => {
     slideOut();
+    Keyboard.dismiss();
     setTimeout(() => {
-      navigate.goBack();
+      navigation.goBack();
     }, 200);
     return true;
-  }, [navigate]);
+  }, [navigation]);
 
   useEffect(() => {
     handleBackPress(handleBackPressFunction);
@@ -47,9 +54,9 @@ export default function CustomHalfModal(props) {
 
   const slideIn = () => {
     Animated.timing(translateY, {
-      toValue: windowDimensions.height - windowDimensions.height * slideHeight,
+      toValue: 0,
       duration: 200,
-      useNativeDriver: false,
+      useNativeDriver: true,
     }).start();
   };
 
@@ -57,70 +64,99 @@ export default function CustomHalfModal(props) {
     Animated.timing(translateY, {
       toValue: windowDimensions.height,
       duration: 200,
-      useNativeDriver: false,
+      useNativeDriver: true,
     }).start();
   };
 
-  const translateY = useRef(
-    new Animated.Value(windowDimensions.height),
-  ).current;
+  const renderContent = () => {
+    switch (contentType) {
+      case 'sendOptions':
+        return <HalfModalSendOptions slideHeight={slideHeight} />;
+      case 'confirmSMS':
+        return (
+          <ConfirmSMSPayment
+            prices={props.route.params?.prices}
+            phoneNumber={props.route.params?.phoneNumber}
+            areaCodeNum={props.route.params?.areaCodeNum}
+            sendTextMessage={props.route.params?.sendTextMessage}
+            page={'sendSMS'}
+          />
+        );
+      case 'confirmVPN':
+        return (
+          <ConfirmVPNPage
+            price={props.route.params?.price}
+            duration={props.route.params?.duration}
+            country={props.route.params?.country}
+            createVPN={props.route.params?.createVPN}
+            slideHeight={slideHeight}
+          />
+        );
+      case 'giftCardConfirm':
+        return (
+          <ConfirmGiftCardPurchase
+            quantity={props.route.params?.quantity}
+            price={props.route.params?.price}
+            productId={props.route.params?.productId}
+            purchaseGiftCard={props.route.params?.purchaseGiftCard}
+            email={props.route.params?.email}
+            blitzUsername={props.route.params?.blitzUsername}
+          />
+        );
+      case 'exportTransactions':
+        return <ConfirmExportPayments />;
+      case 'chatGPT':
+        return (
+          <ConfirmChatGPTPage
+            price={props.route.params?.price}
+            plan={props.route.params?.plan}
+            payForPlan={props.route.params?.payForPlan}
+            slideHeight={slideHeight}
+          />
+        );
+      case 'addContacts':
+        return <AddContactsHalfModal slideHeight={slideHeight} />;
+
+      // Add other cases here...
+      default:
+        return <ThemeText content={'TST'} />;
+    }
+  };
 
   return (
-    <TouchableWithoutFeedback
-      onPress={() => {
-        slideOut();
-        setTimeout(() => {
-          navigate.goBack();
-        }, 200);
-      }}>
-      <View style={styles.contentContainer}>
-        <Animated.View style={{marginTop: translateY}}>
-          {contentType === 'sendOptions' ? (
-            <HalfModalSendOptions slideHeight={slideHeight} />
-          ) : contentType === 'confirmSMS' ? (
-            <ConfirmSMSPayment
-              prices={props.route.params?.prices}
-              phoneNumber={props.route.params?.phoneNumber}
-              areaCodeNum={props.route.params?.areaCodeNum}
-              sendTextMessage={props.route.params?.sendTextMessage}
-              page={'sendSMS'}
-            />
-          ) : contentType === 'confirmVPN' ? (
-            <ConfirmVPNPage
-              price={props.route.params?.price}
-              duration={props.route.params?.duration}
-              country={props.route.params?.country}
-              createVPN={props.route.params?.createVPN}
-              slideHeight={slideHeight}
-            />
-          ) : contentType === 'giftCardConfirm' ? (
-            <ConfirmGiftCardPurchase
-              quantity={props.route.params?.quantity}
-              price={props.route.params?.price}
-              productId={props.route.params?.productId}
-              purchaseGiftCard={props.route.params?.purchaseGiftCard}
-              email={props.route.params?.email}
-              blitzUsername={props.route.params?.blitzUsername}
-            />
-          ) : contentType === 'exportTransactions' ? (
-            <ConfirmExportPayments />
-          ) : contentType === 'chatGPT' ? (
-            <ConfirmChatGPTPage
-              price={props.route.params?.price}
-              plan={props.route.params?.plan}
-              payForPlan={props.route.params?.payForPlan}
-              slideHeight={slideHeight}
-            />
-          ) : (
-            <ThemeText content={'TST'} />
-            // props?.route?.params?.pageContanet
-          )}
-        </Animated.View>
+    <TouchableWithoutFeedback onPress={handleBackPressFunction}>
+      <View style={styles.container}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : null}
+          style={styles.keyboardAvoidingView}>
+          <Animated.View
+            style={[
+              styles.contentContainer,
+              {
+                backgroundColor: backgroundColor,
+                transform: [{translateY}],
+              },
+            ]}>
+            {renderContent()}
+          </Animated.View>
+        </KeyboardAvoidingView>
       </View>
     </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
-  contentContainer: {flex: 1, backgroundColor: COLORS.halfModalBackgroundColor},
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.halfModalBackgroundColor,
+  },
+  keyboardAvoidingView: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
+  contentContainer: {
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    paddingTop: 10,
+  },
 });
