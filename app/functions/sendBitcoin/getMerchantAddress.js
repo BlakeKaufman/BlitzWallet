cd19f009b6b8036b9707cc3f7dcf242f2c35951a
Blake Kaufman <blakekaufman9@gmail.com>
2024-10-31 11:44:57 -0400

    added ability to get merchant QR code details

---
+++ app/functions/sendBitcoin/getMerchantAddress.js
@@ -0,0 +1,38 @@
+const merchants = [
+  {
+    id: 'picknpay',
+    identifierRegex: /(?<identifier>.*za\.co\.electrum\.picknpay.*)/iu,
+    defaultDomain: 'cryptoqr.net',
+    domains: {
+      mainnet: 'cryptoqr.net',
+      signet: 'staging.cryptoqr.net',
+      regtest: 'staging.cryptoqr.net',
+    },
+  },
+  {
+    id: 'ecentric',
+    identifierRegex: /(?<identifier>.*za\.co\.ecentric.*)/iu,
+    defaultDomain: 'cryptoqr.net',
+    domains: {
+      mainnet: 'cryptoqr.net',
+      signet: 'staging.cryptoqr.net',
+      regtest: 'staging.cryptoqr.net',
+    },
+  },
+];
+
+export const convertMerchantQRToLightningAddress = ({qrContent, network}) => {
+  if (!qrContent) {
+    return null;
+  }
+
+  for (const merchant of merchants) {
+    const match = qrContent.match(merchant.identifierRegex);
+    if (match?.groups?.identifier) {
+      const domain = merchant.domains[network] || merchant.defaultDomain;
+      return `${encodeURIComponent(match.groups.identifier)}@${domain}`;
+    }
+  }
+
+  return null;
+};
