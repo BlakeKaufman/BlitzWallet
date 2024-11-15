5870a6d4f6a2b7bc94283d44d03201194ba1bf28
Blake Kaufman <blakekaufman9@gmail.com>
2024-11-15 10:32:31 -0500

    updated notification settings

--- ios/NotificationService/NotificationService.swift
+++ ios/NotificationService/NotificationService.swift
@@ -8,18 +8,21 @@
 import UserNotifications
 import KeychainAccess
 import BreezSDK
+import os.log
+
 
 fileprivate let appGroup = "group.com.blitzwallet.application"
-fileprivate let keychainGroup = "org.reactjs.native.example.BlitzWallet.KeychainSharing"
-fileprivate let accountMnemonic: String = "mnemoinic"
+fileprivate let keychainGroup = "com.blitzwallet.SharedKeychain"
+fileprivate let accountMnemonic: String = "mnemonic"
 fileprivate let accountApiKey: String = "BREEZ_SDK_API_KEY"
 
 class NotificationService: SDKNotificationService {
   // Override the `getConnectRequest` function
   override func getConnectRequest() -> ConnectRequest? {
+    os_log("RUNNING IN GET CONNECT REQUEST")
     // Get the Breez API key from the target bundle's Info.plist
     guard let apiKey = Bundle.main.object(forInfoDictionaryKey: accountApiKey) as? String else {
-      //            os_log(.error, "API key not found")
+      os_log(.error, "API key not found")
       return nil
     }
     var config = defaultConfig(envType: EnvironmentType.production,
@@ -33,20 +36,21 @@ class NotificationService: SDKNotificationService {
       .default.containerURL(forSecurityApplicationGroupIdentifier: appGroup)!
       .appendingPathComponent("breezSdk", isDirectory: true)
       .absoluteString
-    
+
     // Get the mnemonic from the shared keychain using the same
     // service name as the main application
     let service = Bundle.main.bundleIdentifier!.replacingOccurrences(of: ".NotificationService", with: "")
     let keychain = Keychain(service: service, accessGroup: keychainGroup)
     guard let mnemonic = try? keychain.getString(accountMnemonic) else {
-      //            os_log(.error, "Mnemonic not found")
+      os_log(.error, "Mnemonic not found")
       return nil
     }
     // Convert the mnenonic to a seed
     guard let seed = try? mnemonicToSeed(phrase: mnemonic) else {
-      //            os_log(.error, "Invalid seed")
+      os_log(.error, "Invalid seed")
       return nil
     }
+    os_log("SENDING CONNECT REQUEST")
     return ConnectRequest(config: config, seed: seed)
   }
 }
