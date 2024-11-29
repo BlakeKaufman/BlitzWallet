961b6f4852cd61e358dcb4f2a0cf64def8610c49
Blake Kaufman <blakekaufman9@gmail.com>
2024-11-29 09:20:11 -0500

    get appcheck token for app

--- db/initializeFirebase.js
+++ db/initializeFirebase.js
@@ -1,5 +1,6 @@
 import auth from '@react-native-firebase/auth';
 import firestore from '@react-native-firebase/firestore';
+import {firebase} from '@react-native-firebase/app-check';
 
 const db = firestore();
 
@@ -7,13 +8,15 @@ let userAuth;
 
 export async function initializeFirebase() {
   try {
+    // Initialize App Check first
+    await initializeAppCheck();
     // Sign in anonymously
     const userCredential = await auth().signInAnonymously();
 
-    console.log('Signed in anonymously:', userCredential.user.uid);
+    // console.log('Signed in anonymously:', userCredential.user.uid);
 
     // Log Firestore initialization
-    console.log('Firestore initialized:', db);
+    // console.log('Firestore initialized:', db);
     userAuth = userCredential;
 
     // return {db, auth};
@@ -24,3 +27,30 @@ export async function initializeFirebase() {
 }
 
 export {db, userAuth};
+
+const initializeAppCheck = async () => {
+  const provider = firebase.appCheck().newReactNativeFirebaseAppCheckProvider();
+  provider.configure({
+    android: {
+      provider: __DEV__ ? 'debug' : 'playIntegrity',
+      debugToken: __DEV__ ? process.env.FIREBSE_ANDROID_DEBUG_TOKEN : undefined,
+    },
+    apple: {
+      provider: __DEV__ ? 'debug' : 'appAttestWithDeviceCheckFallback',
+      debugToken: __DEV__ ? process.env.FIREBSE_IOS_DEBUG_TOKEN : undefined,
+    },
+    isTokenAutoRefreshEnabled: !__DEV__,
+  });
+  try {
+    await firebase.appCheck().initializeAppCheck({
+      provider,
+      isTokenAutoRefreshEnabled: true,
+    });
+
+    // Optional: Get initial token to verify setup
+    const {token} = await firebase.appCheck().getToken(true);
+    console.log('AppCheck initialized successfully');
+  } catch (error) {
+    console.error('AppCheck initialization failed', error);
+  }
+};
