8c15e06f4472cd1226c3a5597085ca181c0f8af2
Blake Kaufman <blakekaufman9@gmail.com>
2024-12-01 17:27:36 -0500

    added const for blitz fee

--- app/functions/boltz/boltzFeeNew.js
+++ app/functions/boltz/boltzFeeNew.js
@@ -1,14 +1,18 @@
+import {BLITZ_RECEIVE_FEE, BLITZ_SEND_FEE} from '../../constants';
+
 export function calculateBoltzFeeNew(swapAmountSats, swapType, swapInfo) {
   const fee =
     swapType === 'liquid-ln'
       ? Math.round(
           swapInfo.fees.minerFees +
-            swapAmountSats * (swapInfo.fees.percentage / 100),
+            swapAmountSats *
+              ((swapInfo.fees.percentage + BLITZ_SEND_FEE) / 100),
         )
       : Math.round(
           swapInfo.fees.minerFees.claim +
             swapInfo.fees.minerFees.lockup +
-            swapAmountSats * (swapInfo.fees.percentage / 100),
+            swapAmountSats *
+              ((swapInfo.fees.percentage + BLITZ_RECEIVE_FEE) / 100),
         );
 
   return fee;
