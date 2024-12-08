ba3b7e503a269627f3721a3f9edaedacc6d09e08
Blake Kaufman <blakekaufman9@gmail.com>
2024-12-08 16:40:57 -0500

    removed uneeded code

--- app/screens/inAccount/login.js
+++ app/screens/inAccount/login.js
@@ -1,25 +1,12 @@
-import {useContext, useEffect, useRef, useState} from 'react';
-import {AppState, SafeAreaView, StyleSheet, View} from 'react-native';
-
-import {CENTER, COLORS, FONT, SIZES} from '../../constants';
+import {useEffect} from 'react';
 import PinPage from '../../components/admin/loginComponents/pinPage';
-import HomeLogin from '../../components/admin/loginComponents/home';
 import {useGlobalContextProvider} from '../../../context-store/context';
 import {GlobalThemeView} from '../../functions/CustomElements';
-import {WINDOWWIDTH} from '../../constants/theme';
-import {
-  connectToNode,
-  getLocalStorageItem,
-  setLocalStorageItem,
-} from '../../functions';
-import {useWebView} from '../../../context-store/webViewContext';
-import {isMoreThanADayOld} from '../../functions/rotateAddressDateChecker';
-import FullLoadingScreen from '../../functions/CustomElements/loadingScreen';
+import {connectToNode} from '../../functions';
 import useGlobalOnBreezEvent from '../../hooks/globalOnBreezEvent';
 import {getWolletState} from '../../functions/liquidWallet';
 
 export default function AdminLogin({navigation, route}) {
-  const [didUsePin, setDidUsePin] = useState(false);
   const fromBackground = route.params?.fromBackground;
   const {theme} = useGlobalContextProvider();
   const onBreezEvent = useGlobalOnBreezEvent();
@@ -29,34 +16,12 @@ export default function AdminLogin({navigation, route}) {
   }, []);
 
   return (
-    <GlobalThemeView>
-      <View style={[styles.globalContainer]}>
-        {/* {didUsePin && ( */}
-
-        <PinPage
-          navigation={navigation}
-          theme={theme}
-          fromBackground={fromBackground}
-        />
-
-        {/* )} */}
-        {/* {!didUsePin && (
-          <HomeLogin
-            navigation={navigation}
-            theme={theme}
-            setDidUsePin={setDidUsePin}
-            fromBackground={fromBackground}
-          />
-        )} */}
-      </View>
+    <GlobalThemeView useStandardWidth={true}>
+      <PinPage
+        navigation={navigation}
+        theme={theme}
+        fromBackground={fromBackground}
+      />
     </GlobalThemeView>
   );
 }
-
-const styles = StyleSheet.create({
-  globalContainer: {
-    flex: 1,
-    width: WINDOWWIDTH,
-    ...CENTER,
-  },
-});
