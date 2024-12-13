ccf28c546ca0422b7b665a70f3b2bdc6b3ecec3c
Blake Kaufman <blakekaufman9@gmail.com>
2024-12-12 20:14:03 -0500

    updated navigation route path

--- app/components/admin/homeComponents/homeLightning/halfModalSendOptions.js
+++ app/components/admin/homeComponents/homeLightning/halfModalSendOptions.js
@@ -120,7 +120,11 @@ export default function HalfModalSendOptions(props) {
                     },
                   },
                   {
-                    name: 'ManualyEnterSendAddress',
+                    name: 'CustomHalfModal',
+                    params: {
+                      wantedContent: 'manualEnterSendAddress',
+                      sliderHight: 0.5,
+                    },
                   },
                 ],
               });
