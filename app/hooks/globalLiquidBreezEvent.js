c91afcf2eb831a06a56c619605a1da8743a0d92b
Blake Kaufman <blakekaufman9@gmail.com>
2024-12-22 11:54:17 -0500

    created liquid global event listener

---
+++ app/hooks/globalLiquidBreezEvent.js
@@ -0,0 +1,72 @@
+import {
+  PaymentDetailsVariant,
+  PaymentMethod,
+  PaymentState,
+  PaymentType,
+} from '@breeztech/react-native-breez-sdk-liquid';
+import {useGlobalContextProvider} from '../../context-store/context';
+import {useNavigation} from '@react-navigation/native';
+import startLiquidUpdateInterval from '../functions/liquidBackupUpdate';
+
+// SDK events listener
+
+const BLOCKED_PAYMENT_CODES = [];
+//auto channel rebalance
+//APP STORE
+//chatGPT
+//messaging
+//VPN
+
+let intervalId;
+export default function useGlobalLiquidOnBreezEvent() {
+  const {toggleBreezContextEvent, toggleLiquidNodeInformation} =
+    useGlobalContextProvider();
+  const navigate = useNavigation();
+
+  return function onBreezEvent(e) {
+    console.log('Running in breez Liquid event');
+    console.log(e);
+
+    console.log(
+      e.type === 'paymentWaitingConfirmation' || e.type === 'paymentPending',
+    );
+
+    if (
+      e.type === 'paymentWaitingConfirmation' ||
+      e.type === 'paymentPending'
+    ) {
+      if (intervalId) clearInterval(intervalId);
+      intervalId = startLiquidUpdateInterval(toggleLiquidNodeInformation);
+
+      console.log(
+        BLOCKED_PAYMENT_CODES.includes(e.details.description),
+        'BLOCKING NAVIGATION LOGIC',
+      );
+
+      if (BLOCKED_PAYMENT_CODES.includes(e.details.description)) return;
+      navigate.reset({
+        index: 0, // The top-level route index
+        routes: [
+          {
+            name: 'HomeAdmin',
+            params: {screen: 'Home'},
+          },
+          {
+            name: 'ConfirmTxPage',
+            params: {
+              for:
+                e.details.paymentType === PaymentType.SEND
+                  ? 'paymentsucceed'
+                  : 'invoicepaid',
+              information: e?.details,
+              formattingType: 'liquidNode',
+            },
+          },
+        ],
+      });
+    } else if (e.type === 'paymentSucceeded') {
+      if (intervalId) clearInterval(intervalId);
+      intervalId = startLiquidUpdateInterval(toggleLiquidNodeInformation);
+    }
+  };
+}
