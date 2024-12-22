25b137e3ee0d935c4dd70380f71ee755aefc6d15
Blake Kaufman <blakekaufman9@gmail.com>
2024-12-22 10:56:12 -0500

    added pending icon

--- app/functions/CustomElements/Icon.js
+++ app/functions/CustomElements/Icon.js
@@ -745,6 +745,27 @@ export default function Icon({
           <Path d="m13 23.955a1 1 0 0 1 -.089-2 10 10 0 1 0 -10.87-10.865 1 1 0 0 1 -1.992-.18 12 12 0 0 1 23.951 1.09 11.934 11.934 0 0 1 -10.91 11.951c-.03.003-.061.004-.09.004z" />
           <Path d="m12 6a1 1 0 0 0 -1 1v5a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414l-2.707-2.707v-4.586a1 1 0 0 0 -1-1z" />
         </Svg>
+      ) : name === 'pendingTxIcon' ? (
+        <Svg
+          width={width}
+          height={height}
+          viewBox="0 0 84 84"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg">
+          <Circle cx="42" cy="42" r="39" stroke="white" strokeWidth="6" />
+          <Path
+            d="M40 45L57.6373 61.7296"
+            stroke="white"
+            strokeWidth="7"
+            strokeLinecap="round"
+          />
+          <Path
+            d="M40 16V45"
+            stroke="white"
+            strokeWidth="7"
+            strokeLinecap="round"
+          />
+        </Svg>
       ) : (
         <View></View>
       )}
